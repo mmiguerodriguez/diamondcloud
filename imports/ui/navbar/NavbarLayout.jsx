872b3c3aa2c0e06aa905237b9ef84ed5a12c2704
joelsobolmark <joelsobolmark@gmail.com>
2016-07-24 16:40:18 +0000

    Agrego NavbarLayout

---
+++ imports/ui/navbar/NavbarLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class NavbarLayout extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Navbar</h3>
+      </div>
+    );
+  }
+}
