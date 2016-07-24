305bc00cd302fc7b2ca17baedb325699ab8eddc3
joelsobolmark <joelsobolmark@gmail.com>
2016-07-24 18:02:09 +0000

    Add NavbarLayout to AppLayout

--- imports/ui/app/AppLayout.jsx
+++ imports/ui/app/AppLayout.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import NavbarLayout from '../navbar/NavbarLayout.jsx';
+
 export default class AppLayout extends React.Component {
   render() {
     return (
       <div>
+        <NavbarLayout />
         <h1>App</h1>
         { this.props.children }
       </div>
