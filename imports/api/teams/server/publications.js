096a117cfa9555ca84f8d2a086252c5b36f9745d
migu3x <mmiguerodriguez@gmail.com>
2016-07-24 22:58:18 -0300

    Revert to cf69c2e555ff83a34505b061e3a632024bf77ba9

--- imports/api/teams/server/publications.js
+++ imports/api/teams/server/publications.js
@@ -6,22 +6,15 @@ import { Boards } from '../../boards/boards.js';
 
 Meteor.publish('teams.dashboard', function() {
   if (!Meteor.user()) {
-    throw new Meteor.Error('Teams.publication.dashboard.notLoggedIn',
+    throw new Meteor.Error('Teams.publication.dashboard.notLoggedIn', 
     'Must be logged in to view teams.');
   }
 
-<<<<<<< HEAD
-  let teams = Meteor.user().teams({
-    fields: Teams.dashboardFields
-  });
-=======
   let teams = Meteor.user().teams({ 
     fields: Teams.dashboardFields 
   });
   return teams;
->>>>>>> cf69c2e555ff83a34505b061e3a632024bf77ba9
   
-  return teams;
 });
 
 Meteor.publishComposite('teams.team', function(teamId) {
@@ -29,69 +22,30 @@ Meteor.publishComposite('teams.team', function(teamId) {
     throw new Meteor.Error('Teams.publication.team.notLoggedIn',
     'Must be logged in to view teams.');
   }
-
+  
   return {
     find: function() {
-      return Teams.find(teamId, {
-        fields: Teams.teamFields,
+      return Teams.find(teamId, { 
+        fields: Teams.teamFields, 
       });
     },
     children: [
       {
         find: function(team) {
           let boardsIds = [];
-
+          
           team.boards.forEach((board) => {
             boardsIds.push(board._id);
           });
-<<<<<<< HEAD
-
-          let boards = Boards.find({
-            _id: {
-              $in: boardsIds
-            },
-            $or: [
-              {
-                users: {
-                  $elemMatch: {
-                    _id: this.userId,
-                  }
-                }
-              },
-              {
-                isPrivate: false,
-              },
-            ],
-          }, {
-            fields: {
-              _id: 1,
-              name: 1,
-            }
-          });
-
-=======
           
           let boards = Boards.getBoards(boardsIds, this.userId, { _id: 1, name: 1 });
           
->>>>>>> cf69c2e555ff83a34505b061e3a632024bf77ba9
           return boards;
         },
       },
       {
         find: function(team) {
-<<<<<<< HEAD
-          let directChats = DirectChats.find({
-            teamId,
-            users: {
-              $elemMatch: {
-                _id: this.userId,
-              }
-            }
-          });
-          return directChats;
-=======
           return DirectChats.getUserDirectChats(this.userId, teamId);
->>>>>>> cf69c2e555ff83a34505b061e3a632024bf77ba9
         }
       },
     ]
