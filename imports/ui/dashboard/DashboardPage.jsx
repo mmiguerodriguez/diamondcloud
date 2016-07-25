94d3213503532d0527fe85d537e9fa8b32cfe927
migu3x <mmiguerodriguez@gmail.com>
2016-07-25 00:02:51 -0300

    Added subscription to teams.dashboard

--- imports/ui/dashboard/DashboardPage.jsx
+++ imports/ui/dashboard/DashboardPage.jsx
@@ -1,8 +1,13 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 
 import DashboardLayout from './DashboardLayout.jsx';
 
 export default class Dashboard extends React.Component {
+  constructor(){
+    super();
+    Meteor.subscribe('teams.dashboard');
+  }
   render() {
     return (<DashboardLayout teamsCount={3} />);
   }
