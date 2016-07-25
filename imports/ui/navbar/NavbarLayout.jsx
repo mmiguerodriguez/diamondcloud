4e81851bdd71a7c8ac8793997c9e80e0720c7085
joelsobolmark <joelsobolmark@gmail.com>
2016-07-25 20:53:24 +0000

    add logo img, modified main.html, main.scss and NavbarLayout.jsx

--- imports/ui/navbar/NavbarLayout.jsx
+++ imports/ui/navbar/NavbarLayout.jsx
@@ -3,20 +3,30 @@ import React from 'react';
 export default class NavbarLayout extends React.Component {
   render() {
     return (
-      <div>
-        <nav className="navbar header navbar-default">
-          <div className="container-fluid">
-            <div>
-              <a className="navbar-left navbar-img" href="#">
-                <img alt="Brand" src="..." />
-              </a>
-              <a className="navbar-right navbar-img" href="#">
-                <img alt="UserPhoto" className="img-circle" src="//lh3.googleusercontent.com/-ri26AYShk-U/AAAAAAAAAAI/AAAAAAAAAAA/AOkcYIvpY_hVMZyXg-9VyubgjK139xag8A/s32-c-mo/photo.jpg" />
-              </a>
-            </div>
+      <nav className="navbar header">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a href="#"><img src="img/logo.svg" className="logo-photo" /></a>
           </div>
-        </nav>
-      </div>
+          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#" className="li-navbar-text">Dashboard <span className="sr-only">(current)</span></a></li>
+              <li><a href="#" className="li-navbar-text">Help</a></li>
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li>
+                <img src="//lh3.googleusercontent.com/-ri26AYShk-U/AAAAAAAAAAI/AAAAAAAAAAA/AOkcYIvpY_hVMZyXg-9VyubgjK139xag8A/s32-c-mo/photo.jpg" className="img-circle user-photo" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
