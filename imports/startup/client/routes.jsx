41f208066a9065de39cc72f38ddfb98f1a87394f
migu3x <mmiguerodriguez@gmail.com>
2016-07-25 13:03:45 -0300

    Added IndexRoute Landing

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -3,11 +3,13 @@ import { Router, Route, browserHistory } from 'react-router';
 
 // Route components
 import App       from '../../ui/app/AppContainer.jsx';
+import Landing   from '../../ui/landing/LandingPage.jsx';
 import Dashboard from '../../ui/dashboard/DashboardPage.jsx';
 
 export const renderRoutes = () => (
   <Router history={ browserHistory }>
     <Route path="/" component={ App }>
+      <IndexRoute component={ Landing }/>
       <Route path="/dashboard" component={ Dashboard } />
     </Route>
   </Router>
