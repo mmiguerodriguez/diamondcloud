a5baec66be52ad8792fb8168bcb9b081ef187ce9
joelsobolmark <joelsobolmark@gmail.com>
2016-07-25 19:15:28 +0000

    change .css to .scss, add NavbarLayout

--- imports/ui/navbar/NavbarLayout.jsx
+++ imports/ui/navbar/NavbarLayout.jsx
@@ -4,16 +4,16 @@ export default class NavbarLayout extends React.Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-default">
+        <nav className="navbar header navbar-default">
           <div className="container-fluid">
-            <div className="navbar-header">
-              <a className="navbar-brand" href="#">
+            <div>
+              <a className="navbar-left navbar-img" href="#">
                 <img alt="Brand" src="..." />
               </a>
+              <a className="navbar-right navbar-img" href="#">
+                <img alt="UserPhoto" className="img-circle" src="//lh3.googleusercontent.com/-ri26AYShk-U/AAAAAAAAAAI/AAAAAAAAAAA/AOkcYIvpY_hVMZyXg-9VyubgjK139xag8A/s32-c-mo/photo.jpg" />
+              </a>
             </div>
-            <a className="navbar-brand navbar-right" href="#">
-              <img alt="UserPhoto" src="..." />
-            </a>
           </div>
         </nav>
       </div>
