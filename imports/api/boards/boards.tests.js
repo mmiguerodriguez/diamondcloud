9bde4079f44bcc490951f19c6651d153b7072de0
migu3x <mmiguerodriguez@gmail.com>
2016-07-26 16:08:20 -0300

    Made tests with FactoryJs

--- imports/api/boards/boards.tests.js
+++ imports/api/boards/boards.tests.js
@@ -8,83 +8,107 @@ import   faker           from 'faker';
 import { Teams }         from '../teams/teams.js';
 import { Boards }        from './boards.js';
 
+import '../factories/factories.js';
+
 if (Meteor.isServer) {
   describe('Boards', function() {
     describe('Helpers', function() {
-      let user = {
-            _id: Random.id(),
-            emails: [{
-              address: faker.internet.email(), 
-            }],
-          },
-          boards = [{
-            _id: Random.id(),
-            name: faker.lorem.word(),
-            isPrivate: false,
-            users: [
-              { _id: user._id }, 
-              { _id: Random.id() }, 
-              { _id: Random.id() }
-            ],
-            moduleInstances: [],
-            drawings: [],
-            archived: false,
-          }, {
-            _id: Random.id(),
-            name: faker.lorem.word(),
-            isPrivate: false,
-            users: [],
-            moduleInstances: [],
-            drawings: [],
-            archived: false,
-          }],
-          teams = [{
-            _id: Random.id(),
-            name: faker.lorem.word(),
-            plan: 'free',
-            type: faker.lorem.word(),
-            users: [
-              { email: user.emails[0].address, permission: 'owner' },
-              { email: faker.internet.email(), permission: 'member' },
-              { email: faker.internet.email(), permission: 'member' },
-            ],
-            boards: [
-              { _id: boards[0]._id }
-            ],
-            drawings: [],
-            archived: false,
-          }, {
-            _id: Random.id(),
-            name: faker.lorem.word(),
-            plan: 'free',
-            type: faker.lorem.word(),
-            users: [
-              { email: user.emails[0].address, permission: 'owner' },
-              { email: faker.internet.email(), permission: 'member' },
-              { email: faker.internet.email(), permission: 'member' },
-            ],
-            boards: [],
-            drawings: [],
-            archived: false,
-          }];
-      
+      let teams, boards, user;
+      /*
+      boards = [{
+        _id: Random.id(),
+        name: faker.lorem.word(),
+        isPrivate: false,
+        users: [
+          { _id: user._id },
+          { _id: Random.id() },
+          { _id: Random.id() }
+        ],
+        moduleInstances: [],
+        drawings: [],
+        archived: false,
+      }, {
+        _id: Random.id(),
+        name: faker.lorem.word(),
+        isPrivate: false,
+        users: [],
+        moduleInstances: [],
+        drawings: [],
+        archived: false,
+      }],
+      teams = [{
+        _id: Random.id(),
+        name: faker.lorem.word(),
+        plan: 'free',
+        type: faker.lorem.word(),
+        users: [
+          { email: user.emails[0].address, permission: 'owner' },
+          { email: faker.internet.email(), permission: 'member' },
+          { email: faker.internet.email(), permission: 'member' },
+        ],
+        boards: [
+          { _id: boards[0]._id }
+        ],
+        drawings: [],
+        archived: false,
+      }, {
+        _id: Random.id(),
+        name: faker.lorem.word(),
+        plan: 'free',
+        type: faker.lorem.word(),
+        users: [
+          { email: user.emails[0].address, permission: 'owner' },
+          { email: faker.internet.email(), permission: 'member' },
+          { email: faker.internet.email(), permission: 'member' },
+        ],
+        boards: [],
+        drawings: [],
+        archived: false,
+      }];
+  */
+
       beforeEach(function() {
         resetDatabase();
-        sinon.stub(Meteor, 'user', () => user);
-        
-        Meteor.users.insert(user);
-        
-        for(let i = 0; i < boards.length; i++)
-          Boards.insert(boards[i]);
-          
-        for(let i = 0; i < teams.length; i++)
-          Teams.insert(teams[i]);
 
+        user = {
+          _id: Random.id(),
+          emails: [{
+            address: faker.internet.email(),
+          }],
+        };
+        boards = [
+          Factory.create('board'),
+          Factory.create('board')
+        ];
+        teams = [
+          Factory.create('team'),
+          Factory.create('team'),
+        ];
+
+        teams[0].users.push({ email: user.emails[0].address, permission: 'owner' });
+        teams[0].boards.push({ _id: boards[0]._id });
+        teams[1].users.push({ email: user.emails[0].address, permission: 'owner' });
+
+        boards[0].users.push({ _id: user._id });
+
+        resetDatabase();
+
+        Meteor.users.insert(user);
+
+        boards.forEach((board) => {
+          Boards.insert(board);
+        });
+
+        teams.forEach((team) => {
+          Teams.insert(team);
+        });
+
+        sinon.stub(Meteor, 'user', () => user);
       });
       afterEach(function() {
         Meteor.user.restore();
       });
-      
+
       it('should return the team of a board', function() {
         let board = Boards.findOne(boards[0]._id);
         let team = board.team();
@@ -94,7 +118,7 @@ if (Meteor.isServer) {
       it('should not return the team of a board', function() {
         let board = Boards.findOne(boards[1]._id);
         let team = board.team();
-        
+
         chai.assert.isUndefined(team);
       });
     });
