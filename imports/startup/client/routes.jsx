6c49c55ed62642eaa523fb8d4910a6bc8372f9cd
joelsobolmark <joelsobolmark@gmail.com>
2016-07-27 16:36:59 +0000

    Added HelpPage and its route

--- imports/startup/client/routes.jsx
+++ imports/startup/client/routes.jsx
@@ -6,6 +6,7 @@ import App       from '../../ui/app/AppContainer.jsx';
 import Landing   from '../../ui/landing/LandingPage.jsx';
 import Dashboard from '../../ui/dashboard/DashboardPage.jsx';
 import Team      from '../../ui/team/TeamPage.jsx';
+import Help      from '../../ui/help/HelpPage.jsx';
 
 export const renderRoutes = () => (
   <Router history={ browserHistory }>
@@ -13,6 +14,7 @@ export const renderRoutes = () => (
       <IndexRoute component={ Landing }/>
       <Route path="/dashboard" component={ Dashboard } />
       <Route path="/team/:teamId" component={ Team } />
+      <Route path="/help" component={ Help } />
     </Route>
   </Router>
 );
