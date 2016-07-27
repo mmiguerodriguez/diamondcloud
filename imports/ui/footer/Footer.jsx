378f2423b1178b57ed7d0e85e852114485368c65
joelsobolmark <joelsobolmark@gmail.com>
2016-07-27 02:35:34 +0000

    Made footer

--- imports/ui/footer/Footer.jsx
+++ imports/ui/footer/Footer.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 export default class Footer extends React.Component {
   render() {
     return (
-      <div>Footer</div>
+    <footer className="footer">
+      <div className="container-fluid">
+        <div className="row">
+          <p className="col-xs-6 text-muted">Pricing</p>
+          <p className="col-xs-6 text-right text-muted">About Us</p>
+        </div>
+      </div>
+    </footer>
     );
   }
 }
