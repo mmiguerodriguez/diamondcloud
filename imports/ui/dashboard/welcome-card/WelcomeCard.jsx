b4db034919535d301dbcb97fd3d1f8757a5a2caf
joelsobolmark <joelsobolmark@gmail.com>
2016-07-27 02:34:47 +0000

    Almost finished dashboard, tests needed

--- imports/ui/dashboard/welcome-card/WelcomeCard.jsx
+++ imports/ui/dashboard/welcome-card/WelcomeCard.jsx
@@ -3,35 +3,31 @@ import React from 'react';
 export default class WelcomeCard extends React.Component {
   render() {
     return (
-      <div>
-        {
-          this.props.hasTeams === true ? (
-          <div className="welcome-card row">
-            <div className="col-md-7 welcome-text">
-              <h3>
-                <b>Conectate con tu equipo</b>
-              </h3>
-              <p className="text-muted">Aca vamos a tener que poner una bajada para que no quede tan vacio (en gris).</p>
-            </div>
-            <div className="col-md-5">
-              <img src="img/dashboard.png" className="welcome-card-photo" />
-            </div>
-          </div> 
-          ) : (
-          <div className="welcome-card row">
-            <div className="col-md-6 welcome-text">
-              <h3>
-                <b>Empezá a colaborar con tu equipo</b>
-              </h3>
-              <p className="text-muted">Aca vamos a tener que poner una bajada para que no quede tan vacio (en gris).</p>
-            </div>
-            <div className="col-md-6">
-              <img src="img/dashboard.png" className="welcome-card-photo" />
-            </div>
-          </div> 
-          )
-        }
-      </div>
+      this.props.hasTeams === true ? (
+      <div className="welcome-card row">
+        <div className="col-xs-7 welcome-text">
+          <h1>
+            <b>Conectate con tu equipo</b>
+          </h1>
+          <p className="text-muted">Aca vamos a tener que poner una bajada para que no quede tan vacio (en gris).</p>
+        </div>
+        <div className="col-xs-5 welcome-card-photo-div">
+          <img src="img/dashboard.png" className="welcome-card-photo" />
+        </div>
+      </div> 
+      ) : (
+      <div className="welcome-card row">
+        <div className="col-xs-6 welcome-text">
+          <h1>
+            <b>Empezá a colaborar con tu equipo</b>
+          </h1>
+          <p className="text-muted">Aca vamos a tener que poner una bajada para que no quede tan vacio (en gris).</p>
+        </div>
+        <div className="col-xs-6 welcome-card-photo-div">
+          <img src="img/dashboard.png" className="welcome-card-photo" />
+        </div>
+      </div> 
+      )
     );
   }
 }
