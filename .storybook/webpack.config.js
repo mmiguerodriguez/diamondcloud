b8444011a84934029fb711ad59c70d41a67f54d3
migu3x <mmiguerodriguez@gmail.com>
2016-08-01 16:44:01 -0300

    Fixed images. Added scripts and styles. Modified storybook call params

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -9,6 +9,14 @@ module.exports = {
         exclude: /node_modules/,
         include: path.resolve(__dirname, '../../')
       },
+      {
+        test: /\.(jpe?g|jpg|gif|png|woff|woff2|eot|ttf|svg)$/,
+        loaders: [
+          'url?limit=10000',
+          'img'
+        ],
+        include: __dirname
+      }
     ]
   }
 };
