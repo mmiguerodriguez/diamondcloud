f14306ecc8ef25676a41dea03e81703fc82654aa
Joel Sobol Mark <joelsobolmark@gmail.com>
2016-08-04 09:36:10 -0300

    Modified server layer of publication modules api

--- imports/api/module-instances/server/publications.js
+++ imports/api/module-instances/server/publications.js
@@ -1,36 +1,59 @@
 import { Meteor } from 'meteor/meteor';
 import { ModuleInstances } from '../module-instances.js';
 
-Meteor.publish('ModuleInstances.database', function(collection, condition) {
-  
-  return ModuleInstances.aggregate(
-    [
-      {
-        $project: {
-          [collection]: `$database.${collection}`
-        }
-      },
-      {
-        $project: {
-          [collection]: {
-            $filter: {
-              input: `$${collection}`,
-              as: 'element',
-              cond: condition
+Meteor.publish('ModuleInstances.database', function(obj, teamId) {
+
+  let boards = this.user.boards(teamId, { _id: 1 });
+  boards = boards.map((board) => {
+    return board._id;
+  });
+
+  let groups = this.user.groups(teamId, { _id: 1});
+  groups = groups.map((group) => {
+    return group._id;
+  });
+
+  let generateTree = (params, upperParams) => {
+    let children = [];
+    params.children.forEach((child) => {
+      children.push(generateTree(child, params));
+    });
+    let result = {
+      find: () => {
+        return ModuleInstances.aggregate(
+          [
+            {
+              $project: {
+                [params.collection]: `$database.${params.collection}`
+              }
+            },
+            {
+              $project: {
+                [params.collection]: {
+                  $filter: {
+                    input: `$${params.collection}`,
+                    as: 'element',
+                    cond: params.condition
+                  }
+                }
+              }
+            },
+            {
+              $match: {
+                $or: [
+                  { 'todos.visibleBy': null },
+                  { 'todos.visibleBy.userId': this.userId },
+                  { 'todos.visibleBy.boardId': { $in: boards } },
+                  { 'todos.visibleBy.groupId': { $in: groups } }
+                ]
+              }
             }
-          }
-        }
-      },
-      {
-        $match: {
-          $or: [
-            { 'todos.visibleBy': null },
-            { 'todos.visibleBy.userId': this.userId },
-            { 'todos.visibleBy.boardId': { $in: ['General', '.'] } },
-            { 'todos.visibleBy.groupId': { $in: ['sas','.'] } }
           ]
-        }
-      }
-    ]
-  )._batch;
+        );
+      },
+      children
+    };
+  };
+
+  return generateTree(obj);
 });
