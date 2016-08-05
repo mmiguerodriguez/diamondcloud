1ae4cc798804c2873e598b9448083fcdb735ed02
Ryanitz <ryanitzcovitz@gmail.com>
2016-08-05 18:19:23 -0300

    Fixed sidebar icons and text.

--- imports/ui/team/sidebar/SidebarLayout.jsx
+++ imports/ui/team/sidebar/SidebarLayout.jsx
@@ -7,20 +7,20 @@ export default class SidebarLayout extends React.Component {
     return (
       <div className="sidebar">
         <div className="item">
-          <img src="/img/sidebar/boards.svg" width="48px" />
+          <img src="/img/sidebar/boards.svg" width="32px" />
           <p className="text">Boards</p>
         </div>
         <div className="item">
-          <img src="/img/sidebar/modules.svg" width="48px" />
+          <img src="/img/sidebar/modules.svg" width="32px" />
           <p className="text">Módulos</p>
         </div>
         <div className="item">
-          <img src="/img/sidebar/messages.svg" width="48px" />
+          <img src="/img/sidebar/messages.svg" width="32px" />
           <p className="text">Mensajes</p>
         </div>
 
         <div className="item bottom">
-          <img src="/img/sidebar/config.svg" width="48px" />
+          <img src="/img/sidebar/config.svg" width="32px" />
         </div>
 
         <ModulesCollapsible />
