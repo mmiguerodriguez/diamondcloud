0f32c5bc05688d2d831b6736585775e8f4a2b7a1
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-08-17 18:45:46 -0300

    Search a team on /dashboard

--- imports/ui/dashboard/teams/TeamsLayout.jsx
+++ imports/ui/dashboard/teams/TeamsLayout.jsx
@@ -3,23 +3,11 @@ import React from 'react';
 import TeamCard from './team-card/TeamCard.jsx';
 
 export default class TeamsLayout extends React.Component {
-  renderTeams(){
-    if(this.props.hasTeams) {
-      return this.props.teams.map((team) => {
-        return (
-          <TeamCard
-            key={ team._id }
-            team={ team }
-            hasTeams={ this.props.hasTeams }
-            openConfigTeamModal={ this.props.openConfigTeamModal }
-          />
-        );
-      });
-    } else {
-      return (
-        <TeamCard hasTeams={ this.props.hasTeams }/>
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+    };
   }
   render() {
     return (
@@ -33,9 +21,11 @@ export default class TeamsLayout extends React.Component {
           <div className="col-md-6">
             <div className="input-group col-md-6 search-teams" role="button">
               <input  id="searchTeams"
+                      value={ this.state.search }
                       className="form-control input"
                       placeholder="Busca tus equipos"
-                      type="search" />
+                      type="search"
+                      onChange={ this.handleChange.bind(this) }/>
               <div className="input-group-addon icon">
                 <img src="/img/search.svg"
                      width="24px" />
@@ -48,6 +38,29 @@ export default class TeamsLayout extends React.Component {
         </div>
       </div>
     );
+  }
+  handleChange(event) {
+    this.setState({
+      search: event.target.value,
+    });
+  }
+  renderTeams(){
+    if(this.props.hasTeams) {
+      return this.props.teams.map((team) => {
+        return team.name.toLowerCase().indexOf(this.state.search.toLowerCase()) > -1 ? (
+          <TeamCard
+            key={ team._id }
+            team={ team }
+            hasTeams={ this.props.hasTeams }
+            openConfigTeamModal={ this.props.openConfigTeamModal }
+          />
+      ) : ( null );
+      });
+    } else {
+      return (
+        <TeamCard hasTeams={ this.props.hasTeams }/>
+      );
+    }
   }
 }
 
