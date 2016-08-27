db8926f7c33b5e43b2d28bc3a1a43998fe3cf0e0
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-08-27 13:13:53 -0300

    Started using DiamondAPI for modules - Made Post-it module work with a setInterval to receive data since   API subscriptions aren't ready.

--- public/modules/Jjwjg6gouWLXhMGKW/main.js
+++ public/modules/Jjwjg6gouWLXhMGKW/main.js
@@ -1,16 +1,73 @@
-var resize;
-
 window.onload = () => {
-  console.log('I am a module and I just loaded');
+  console.log('Module loaded, grabbing data...');
+  getData((error, result) => {
+    console.log('Grabbed data...');
+    if(!result) {
+      insertStartupData((error, result) => {
+        console.log('Inserted new data since there was none...');
+      });
+    } else {
+      console.log('Found data, handling it to the inputs...');
+      let postIt = result[0];
+      handleNewData(postIt);
+    }
+  });
 };
-console.log(window.DiamondAPI);
-
 window.onresize = () => {
-  clearTimeout(resize);
-  resize = setTimeout(() => {
-    console.log('Resized module', {
-      width: this.innerWidth,
-      height: this.innerHeight,
-    });
-  }, 500);
+  console.log('Resized module...');
 };
+//TODO: Remove this once subscription is ready
+window.setInterval(() => {
+  getData((error, result) => {
+    if(result) {
+      let postIt = result[0]; // [{ _id, title, description }]
+      handleNewData(postIt);
+    }
+  });
+}, 2000);
+
+// First insert data
+function insertStartupData(callback) {
+  window.DiamondAPI.insert({
+    collection: 'postIt',
+    obj: {
+      title: '',
+      description: '',
+    },
+    visibleBy: [],
+    callback,
+  });
+}
+
+// API methods
+function getData(callback) {
+  window.DiamondAPI.get({
+    collection: 'postIt',
+    filter: {},
+    callback,
+  });
+}
+function updateData(object) {
+  window.DiamondAPI.update(object);
+}
+
+// Module methods
+function updateInput(e, which) {
+  updateData({
+    collection: 'postIt',
+    filter: {},
+    updateQuery: {
+      $set: {
+        [which]: e.value,
+      }
+    }
+  });
+}
+function handleNewData(data) {
+  pushData('title', data.title);
+  pushData('description', data.description);
+
+  function pushData(e, value) {
+    document.getElementById(e).value = value;
+  }
+}
