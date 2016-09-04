2b2976ae1668fd71ee907cb97431aa5e86c54fc8
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-09-04 17:55:56 -0300

    Fixed directChats tests describe and descriptions

--- imports/api/direct-chats/direct-chats.tests.js
+++ imports/api/direct-chats/direct-chats.tests.js
@@ -12,143 +12,145 @@ import '../factories/factories.js';
 
 if (Meteor.isServer) {
   describe('DirectChats', function() {
-    let users, teams, directChats;
-    function getNotifications(directChatId, userId) {
-      return DirectChats.findOne(directChatId).users.find((user) => {
-        return user._id !== userId;
-      }).notifications;
-    }
+    describe('Helpers', function() {
+      let users, teams, directChats;
+      function getNotifications(directChatId, userId) {
+        return DirectChats.findOne(directChatId).users.find((user) => {
+          return user._id !== userId;
+        }).notifications;
+      }
 
-    beforeEach(function() {
-      resetDatabase();
+      beforeEach(function() {
+        resetDatabase();
 
-      users = [
-        Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
-        Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
-        Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
-        Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
-      ];
-      teams = [
-        Factory.create('team', {
-          users: [
-            { email: users[0].emails[0].address, permission: 'owner' },
-            { email: users[1].emails[0].address, permission: 'member' },
-          ],
-        }),
-        Factory.create('team', {
-          users: [
-            { email: users[1].emails[0].address, permission: 'owner' },
-            { email: users[2].emails[0].address, permission: 'member' },
-          ],
-        }),
-        Factory.create('team', {
-          users: [
-            { email: users[2].emails[0].address, permission: 'owner' },
-            { email: users[3].emails[0].address, permission: 'member' },
-          ],
-        }),
-        Factory.create('team', {
-          users: [
-            { email: users[0].emails[0].address, permission: 'owner' },
-            { email: users[3].emails[0].address, permission: 'member' },
-          ],
-        }),
-      ];
-      directChats = [
-        Factory.create('directChat', {
-          teamId: teams[0]._id,
-          users: [
-            { _id: users[0]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-            { _id: users[1]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-          ]
-        }),
-        Factory.create('directChat', {
-          teamId: teams[1]._id,
-          users: [
-            { _id: users[1]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-            { _id: users[2]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-          ]
-        }),
-        Factory.create('directChat', {
-          teamId: teams[2]._id,
-          users: [
-            { _id: users[2]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-            { _id: users[3]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-          ]
-        }),
-        Factory.create('directChat', {
-          teamId: teams[3]._id,
-          users: [
-            { _id: users[0]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-            { _id: users[3]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
-          ]
-        }),
-      ];
+        users = [
+          Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
+          Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
+          Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
+          Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
+        ];
+        teams = [
+          Factory.create('team', {
+            users: [
+              { email: users[0].emails[0].address, permission: 'owner' },
+              { email: users[1].emails[0].address, permission: 'member' },
+            ],
+          }),
+          Factory.create('team', {
+            users: [
+              { email: users[1].emails[0].address, permission: 'owner' },
+              { email: users[2].emails[0].address, permission: 'member' },
+            ],
+          }),
+          Factory.create('team', {
+            users: [
+              { email: users[2].emails[0].address, permission: 'owner' },
+              { email: users[3].emails[0].address, permission: 'member' },
+            ],
+          }),
+          Factory.create('team', {
+            users: [
+              { email: users[0].emails[0].address, permission: 'owner' },
+              { email: users[3].emails[0].address, permission: 'member' },
+            ],
+          }),
+        ];
+        directChats = [
+          Factory.create('directChat', {
+            teamId: teams[0]._id,
+            users: [
+              { _id: users[0]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+              { _id: users[1]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+            ]
+          }),
+          Factory.create('directChat', {
+            teamId: teams[1]._id,
+            users: [
+              { _id: users[1]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+              { _id: users[2]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+            ]
+          }),
+          Factory.create('directChat', {
+            teamId: teams[2]._id,
+            users: [
+              { _id: users[2]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+              { _id: users[3]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+            ]
+          }),
+          Factory.create('directChat', {
+            teamId: teams[3]._id,
+            users: [
+              { _id: users[0]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+              { _id: users[3]._id, notifications: faker.random.number({ min: 0, max: 20 }) },
+            ]
+          }),
+        ];
 
-      resetDatabase();
+        resetDatabase();
 
-      users.forEach((user) => Meteor.users.insert(user));
-      teams.forEach((team) => Teams.insert(team));
-      directChats.forEach((directChat) => DirectChats.insert(directChat));
-    });
+        users.forEach((user) => Meteor.users.insert(user));
+        teams.forEach((team) => Teams.insert(team));
+        directChats.forEach((directChat) => DirectChats.insert(directChat));
+      });
 
-    afterEach(function() {
+      afterEach(function() {
 
-    });
+      });
 
-    it('should check if a direct-chat is valid', function() {
-      chai.assert.isTrue(DirectChats.isValid(directChats[0]._id, users[0]._id));
-      chai.assert.isTrue(DirectChats.isValid(directChats[0]._id, users[1]._id));
-      chai.assert.isFalse(DirectChats.isValid(directChats[0]._id, users[2]._id));
-      chai.assert.isFalse(DirectChats.isValid(directChats[0]._id, users[3]._id));
+      it('should return if a direct-chat is valid', function() {
+        chai.assert.isTrue(DirectChats.isValid(directChats[0]._id, users[0]._id));
+        chai.assert.isTrue(DirectChats.isValid(directChats[0]._id, users[1]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[0]._id, users[2]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[0]._id, users[3]._id));
 
-      chai.assert.isFalse(DirectChats.isValid(directChats[1]._id, users[0]._id));
-      chai.assert.isTrue(DirectChats.isValid(directChats[1]._id, users[1]._id));
-      chai.assert.isTrue(DirectChats.isValid(directChats[1]._id, users[2]._id));
-      chai.assert.isFalse(DirectChats.isValid(directChats[1]._id, users[3]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[1]._id, users[0]._id));
+        chai.assert.isTrue(DirectChats.isValid(directChats[1]._id, users[1]._id));
+        chai.assert.isTrue(DirectChats.isValid(directChats[1]._id, users[2]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[1]._id, users[3]._id));
 
-      chai.assert.isFalse(DirectChats.isValid(directChats[2]._id, users[0]._id));
-      chai.assert.isFalse(DirectChats.isValid(directChats[2]._id, users[1]._id));
-      chai.assert.isTrue(DirectChats.isValid(directChats[2]._id, users[2]._id));
-      chai.assert.isTrue(DirectChats.isValid(directChats[2]._id, users[3]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[2]._id, users[0]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[2]._id, users[1]._id));
+        chai.assert.isTrue(DirectChats.isValid(directChats[2]._id, users[2]._id));
+        chai.assert.isTrue(DirectChats.isValid(directChats[2]._id, users[3]._id));
 
-      chai.assert.isTrue(DirectChats.isValid(directChats[3]._id, users[0]._id));
-      chai.assert.isFalse(DirectChats.isValid(directChats[3]._id, users[1]._id));
-      chai.assert.isFalse(DirectChats.isValid(directChats[3]._id, users[2]._id));
-      chai.assert.isTrue(DirectChats.isValid(directChats[3]._id, users[3]._id));
-    });
-    it('should get user direct-chats', function() {
-      let foundChats = [
-        ...DirectChats.getUserDirectChats(users[0]._id, teams[0]._id).fetch(),
-        ...DirectChats.getUserDirectChats(users[0]._id, teams[3]._id).fetch(),
-      ];
+        chai.assert.isTrue(DirectChats.isValid(directChats[3]._id, users[0]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[3]._id, users[1]._id));
+        chai.assert.isFalse(DirectChats.isValid(directChats[3]._id, users[2]._id));
+        chai.assert.isTrue(DirectChats.isValid(directChats[3]._id, users[3]._id));
+      });
+      it('should return user direct-chats', function() {
+        let foundChats = [
+          ...DirectChats.getUserDirectChats(users[0]._id, teams[0]._id).fetch(),
+          ...DirectChats.getUserDirectChats(users[0]._id, teams[3]._id).fetch(),
+        ];
 
-      let realChats = [
-        directChats[0],
-        directChats[3],
-      ];
+        let realChats = [
+          directChats[0],
+          directChats[3],
+        ];
 
-      chai.assert.deepEqual(foundChats, realChats);
-    });
-    it('should add a notification of user direct-chat', function() {
-      let startNotifications, endNotifications;
+        chai.assert.deepEqual(foundChats, realChats);
+      });
+      it('should add a notification of user direct-chat', function() {
+        let startNotifications, endNotifications;
 
-      startNotifications = getNotifications(directChats[0]._id, users[0]._id);
-      DirectChats.addNotification(directChats[0]._id, users[0]._id);
-      endNotifications = getNotifications(directChats[0]._id, users[0]._id);
+        startNotifications = getNotifications(directChats[0]._id, users[0]._id);
+        DirectChats.addNotification(directChats[0]._id, users[0]._id);
+        endNotifications = getNotifications(directChats[0]._id, users[0]._id);
 
-      chai.assert.notEqual(startNotifications, endNotifications);
-      chai.assert.equal(startNotifications + 1, endNotifications);
-    });
-    it('should reset notifications of user direct-chat', function() {
-      let startNotifications, endNotifications;
+        chai.assert.notEqual(startNotifications, endNotifications);
+        chai.assert.equal(startNotifications + 1, endNotifications);
+      });
+      it('should reset notifications of user direct-chat', function() {
+        let startNotifications, endNotifications;
 
-      startNotifications = getNotifications(directChats[0]._id, users[0]._id);
-      DirectChats.resetNotifications(directChats[0]._id, users[0]._id);
-      endNotifications = getNotifications(directChats[0]._id, users[0]._id);
+        startNotifications = getNotifications(directChats[0]._id, users[0]._id);
+        DirectChats.resetNotifications(directChats[0]._id, users[0]._id);
+        endNotifications = getNotifications(directChats[0]._id, users[0]._id);
 
-      chai.assert.notEqual(startNotifications, endNotifications);
-      chai.assert.equal(endNotifications, 0);
+        chai.assert.notEqual(startNotifications, endNotifications);
+        chai.assert.equal(endNotifications, 0);
+      });
     });
   });
 }
--- imports/api/direct-chats/methods.tests.js
+++ imports/api/direct-chats/methods.tests.js
@@ -11,71 +11,69 @@ import { Teams }            from '../teams/teams.js';
 
 if (Meteor.isServer) {
   describe('DirectChats', function() {
-    describe('Methods', function() {
-      let users, team;
+    let users, team;
 
-      beforeEach(function() {
-        resetDatabase();
+    beforeEach(function() {
+      resetDatabase();
 
-        users = [
-          Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
-          Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
-        ];
-        team = Factory.create('team');
+      users = [
+        Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
+        Factory.create('user', { _id: Random.id(), emails: [{ address: faker.internet.email() }] }),
+      ];
+      team = Factory.create('team');
 
-        team.users = [
-          { email: users[0].emails[0].address, permission: 'owner' },
-          { email: users[1].emails[0].address, permission: 'member' },
-        ];
+      team.users = [
+        { email: users[0].emails[0].address, permission: 'owner' },
+        { email: users[1].emails[0].address, permission: 'member' },
+      ];
 
-        resetDatabase();
+      resetDatabase();
 
-        sinon.stub(Meteor, 'user', () => users[0]);
-        sinon.stub(Meteor, 'userId', () => users[0]._id);
+      sinon.stub(Meteor, 'user', () => users[0]);
+      sinon.stub(Meteor, 'userId', () => users[0]._id);
 
-        users.forEach((user) => Meteor.users.insert(user));
+      users.forEach((user) => Meteor.users.insert(user));
 
-        Teams.insert(team);
+      Teams.insert(team);
+    });
+
+    afterEach(function() {
+      Meteor.user.restore();
+      Meteor.userId.restore();
+    });
+
+    it('should create a direct chat', function(done) {
+      let args,
+          result,
+          expect;
+
+      args = {
+        teamId: team._id,
+        userId: users[1]._id,
+      };
+      expect = {
+        teamId: args.teamId,
+        users: [
+          { _id: users[0]._id, notifications: 0 },
+          { _id: users[1]._id, notifications: 0 },
+        ],
+      };
+
+      createDirectChat.call(args, (err, res) => {
+        if(err) throw new Meteor.Error(err);
+        result = res;
+
+        expect._id = result._id;
       });
 
-      afterEach(function() {
-        Meteor.user.restore();
-        Meteor.userId.restore();
+      createDirectChat.call(args, (err, res) => {
+        if(err) {
+          chai.assert.equal(err.details, 'chat_exists');
+        }
       });
 
-      it('should create a direct chat', function(done) {
-        let args,
-            result,
-            expect;
-
-        args = {
-          teamId: team._id,
-          userId: users[1]._id,
-        };
-        expect = {
-          teamId: args.teamId,
-          users: [
-            { _id: users[0]._id, notifications: 0 },
-            { _id: users[1]._id, notifications: 0 },
-          ],
-        };
-
-        createDirectChat.call(args, (err, res) => {
-          if(err) throw new Meteor.Error(err);
-          result = res;
-
-          expect._id = result._id;
-        });
-
-        createDirectChat.call(args, (err, res) => {
-          if(err) {
-            chai.assert.equal(err.details, 'chat_exists');
-          }
-        });
-
-        chai.assert.deepEqual(result, expect);
-        done();
-      });
+      chai.assert.deepEqual(result, expect);
+      done();
     });
   });
 }
