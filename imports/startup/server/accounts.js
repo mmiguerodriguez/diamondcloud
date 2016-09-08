3d519d7acf763f57273fb3faaadd46b66f907562
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-09-07 21:22:38 -0300

    Added automatic add of secret and client keys for loginWithGoogle

---
+++ imports/startup/server/accounts.js
@@ -0,0 +1,8 @@
+ServiceConfiguration.configurations.remove({
+  service: 'google'
+});
+ServiceConfiguration.configurations.insert({
+  service: 'google',
+  clientId: '624318008240-lkme1mqg4ist618vrmj70rkqbo95njnd.apps.googleusercontent.com',
+  secret: 'kDVxT5rJsvROOlz4rAWE_tzY'
+});
