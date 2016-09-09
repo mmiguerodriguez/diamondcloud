bcbe0952f2e877bba638a4cd5ed17965f3aff07b
Ryanitz <ryanitzcovitz@gmail.com>
2016-09-09 12:01:55 +0000

    Added a componentDidMount check on Landing to redirect to dashboard if user is logged in

--- imports/ui/landing/LandingPage.jsx
+++ imports/ui/landing/LandingPage.jsx
@@ -1,10 +1,17 @@
-import { Meteor } from 'meteor/meteor';
-import React from 'react';
+import { Meteor }          from 'meteor/meteor';
 
-import LandingLayout from './LandingLayout.jsx';
+import React               from 'react';
+import { browserHistory }  from 'react-router';
+
+import LandingLayout       from './LandingLayout.jsx';
 
 export default class Landing extends React.Component {
   render() {
     return (<LandingLayout />);
+  }
+  componentWillMount() {
+    if(Meteor.user()) {
+      browserHistory.push('/dashboard');
+    }
   }
 }
