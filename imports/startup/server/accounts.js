9e06486ec0c21feda649cb31fcc189d001d441db
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-09-10 23:03:50 -0300

    Changed accounts ServiceConfiguration

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,8 +1,7 @@
-ServiceConfiguration.configurations.remove({
-  service: 'google'
-});
-ServiceConfiguration.configurations.insert({
-  service: 'google',
-  clientId: '624318008240-lkme1mqg4ist618vrmj70rkqbo95njnd.apps.googleusercontent.com',
-  secret: 'kDVxT5rJsvROOlz4rAWE_tzY'
+ServiceConfiguration.configurations.upsert({ service: 'google' }, {
+  $set: {
+    clientId: '624318008240-lkme1mqg4ist618vrmj70rkqbo95njnd.apps.googleusercontent.com',
+    secret: 'kDVxT5rJsvROOlz4rAWE_tzY',
+    loginStyle: 'redirect',
+  }
 });
