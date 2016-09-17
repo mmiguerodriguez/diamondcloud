cf0fa341c45785a31ed10287977a1176960743ff
raikhen <dylanfridman@gmail.com>
2016-09-17 18:32:29 -0300

    Made a settings file which we will all use or die

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -1,7 +1,7 @@
 ServiceConfiguration.configurations.upsert({ service: 'google' }, {
   $set: {
-    clientId: '624318008240-lkme1mqg4ist618vrmj70rkqbo95njnd.apps.googleusercontent.com',
-    secret: 'kDVxT5rJsvROOlz4rAWE_tzY',
+    clientId: Meteor.settings.private.oAuth.google.clientId,
+    secret: Meteor.settings.private.oAuth.google.secret,
     loginStyle: 'redirect',
   }
 });
