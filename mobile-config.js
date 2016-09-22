92f540a427895d597970a56913e1bbdc6240a9d3
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-09-22 15:14:28 +0000

    Added splash-screens and icons for mobile-configuration

--- mobile-config.js
+++ mobile-config.js
@@ -18,11 +18,12 @@ App.icons({
   'ipad_2x': 'public/img/logo.png', // 76x76
 
   // Android
-  'android_mdpi': 'public/img/logo.png', // 48x48
-  'android_hdpi': 'public/img/logo.png', // 72x72
-  'android_xhdpi': 'public/img/logo.png', // 96x96
-  'android_xxhdpi': 'public/img/logo.png',
-  'android_xxxhdpi': 'public/img/logo.png',
+  'android_ldpi': 'resources/icons/icon-36x36.png',
+  'android_mdpi': 'resources/icons/icon-48x48.png',
+  'android_hdpi': 'resources/icons/icon-72x72.png',
+  'android_xhdpi': 'resources/icons/icon-96x96.png',
+  'android_xxhdpi': 'resources/icons/icon-144x144.png',
+  'android_xxxhdpi': 'resources/icons/icon-192x192.png',
 });
 
 App.launchScreens({
@@ -32,21 +33,24 @@ App.launchScreens({
   'iphone6': 'public/img/logo.png', // 375x667
   'iphone6p_portrait': 'public/img/logo.png', // 414x736
   'iphone6p_landscape': 'public/img/logo.png', // 736x414
-
   'ipad_portrait': 'public/img/logo.png', // 768x1024
   'ipad_portrait_2x': 'public/img/logo.png', // 768x1024
   'ipad_landscape': 'public/img/logo.png', // 1024x768
   'ipad_landscape_2x': 'public/img/logo.png', // 1024x768
 
   // Android
-  'android_mdpi_portrait': 'public/img/logo.png', // 320x480
-  'android_mdpi_landscape': 'public/img/logo.png', // 480x320
-  'android_hdpi_portrait': 'public/img/logo.png', // 480x800
-  'android_hdpi_landscape': 'public/img/logo.png', // 800x480
-  'android_xhdpi_portrait': 'public/img/logo.png', // 720x1280
-  'android_xhdpi_landscape': 'public/img/logo.png', // 1280x720
-  'android_xxhdpi_portrait': 'public/img/logo.png',
-  'android_xxhdpi_landscape': 'public/img/logo.png',
+  'android_ldpi_portrait': 'resources/splash/splash-230x320.png',
+  'android_ldpi_landscape': 'resources/splash/splash-320x230.png',
+  'android_mdpi_portrait': 'resources/splash/splash-320x480.png',
+  'android_mdpi_landscape': 'resources/splash/splash-480x320.png',
+  'android_hdpi_portrait': 'resources/splash/splash-480x800.png',
+  'android_hdpi_landscape': 'resources/splash/splash-800x480.png',
+  'android_xhdpi_portrait': 'resources/splash/splash-720x1280.png',
+  'android_xhdpi_landscape': 'resources/splash/splash-1280x720.png',
+  'android_xxhdpi_portrait': 'resources/splash/splash-960x1600.png',
+  'android_xxhdpi_landscape': 'resources/splash/splash-1600x960.png',
+  'android_xxxhdpi_portrait': 'resources/splash/splash-1280x1920.png',
+  'android_xxxhdpi_landscape': 'resources/splash/splash-1920x1280.png',
 });
 
 // Set PhoneGap/Cordova preferences
