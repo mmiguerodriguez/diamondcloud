c9fbe690f3713aec4637eecf3bd6bc51ed0d43c4
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-09-23 22:07:48 +0000

    Fixed popover to work on mobile

--- imports/startup/client/react-popover.js
+++ imports/startup/client/react-popover.js
@@ -3,36 +3,36 @@
  * http://jsfiddle.net/spicyj/q6hj7/
  * http://stackoverflow.com/questions/20033522/rendering-a-react-component-inside-a-bootstrap-popover
  */
-
 import ReactDOM from 'react-dom';
-// fix jquery import vieja
-/*
-$.extend($.fn.popover.Constructor.DEFAULTS, {
-  react: false
-});
 
-let oldSetContent = $.fn.popover.Constructor.prototype.setContent;
-$.fn.popover.Constructor.prototype.setContent = function() {
-  if (!this.options.react) {
-    return oldSetContent.call(this);
-  }
-
-  let $tip = this.tip();
-  let title = this.getTitle();
-  let content = this.getContent();
-
-  $tip.removeClass('fade top bottom left right in');
-
-  // If we've already rendered, there's no need to render again
-  if (!$tip.find('.popover-content').html()) {
-    // Render title, if any
-    let $title = $tip.find('.popover-title');
-    if(title) {
-      ReactDOM.render(title, $title[0]);
-    } else {
-      $title.hide();
+// Extend only if jQuery object exists and $.n (jquery functions) is an object
+if(typeof $.fn === 'object' && typeof jQuery !== 'undefined') {
+  $.extend($.fn.popover.Constructor.DEFAULTS, {
+    react: false
+  });
+  
+  let oldSetContent = $.fn.popover.Constructor.prototype.setContent;
+  $.fn.popover.Constructor.prototype.setContent = function() {
+    if (!this.options.react) {
+      return oldSetContent.call(this);
     }
-    ReactDOM.render(content,  $tip.find('.popover-content')[0]);
-  }
-};
-*/
+  
+    let $tip = this.tip();
+    let title = this.getTitle();
+    let content = this.getContent();
+  
+    $tip.removeClass('fade top bottom left right in');
+  
+    // If we've already rendered, there's no need to render again
+    if (!$tip.find('.popover-content').html()) {
+      // Render title, if any
+      let $title = $tip.find('.popover-title');
+      if(title) {
+        ReactDOM.render(title, $title[0]);
+      } else {
+        $title.hide();
+      }
+      ReactDOM.render(content,  $tip.find('.popover-content')[0]);
+    }
+  };
+}
