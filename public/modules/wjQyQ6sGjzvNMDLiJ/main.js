d1cd5ddcc40547e5735957bb7ce237f896927177
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-09-23 21:25:10 +0000

    Minor fixes

--- public/modules/wjQyQ6sGjzvNMDLiJ/main.js
+++ public/modules/wjQyQ6sGjzvNMDLiJ/main.js
@@ -1,68 +1,59 @@
-var teamData;
-var subscriptions = [];
+var openFABMenu = function (btn) {
+  let $this = btn;
+  if ($this.hasClass('active') === false) {
 
-window.onload = () => {
-  DiamondAPI.get({
-    collection: 'postIt',
-    filter: {},
-    callback: (error, result) => {
-      if (error) throw error;
-      console.log('Grabbed data...');
-      if(!result) {
-        insertStartupData((error, result) => {
-          if (!!error) throw error;
-          console.log('Inserted new data since there was none...');
-        });
-      } else {
-        handleNewData(result[0]);
-      }
-    },
-  });
+    // Get direction option
+    var horizontal = $this.hasClass('horizontal');
+    var offsetY, offsetX;
 
-  let subscription = DiamondAPI.subscribe({
-    request: {
-      collection: 'postIt',
-    },
-    callback: (err, res) => {
-      if (!!err) throw console.error(err);
-      console.log('New data incoming...', res);
-      handleNewData(res.postIt[0]);
-    },
-  });
+    if (horizontal === true) {
+      offsetX = 40;
+    } else {
+      offsetY = 40;
+    }
 
-  subscriptions.push(subscription);
-  teamData = DiamondAPI.getTeamData();
+    $this.addClass('active');
+    $this.find('ul .btn-floating').velocity(
+      { scaleY: ".4", scaleX: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px'},
+      { duration: 0 });
+
+    var time = 0;
+    $this.find('ul .btn-floating').reverse().each( function () {
+      $(this).velocity(
+        { opacity: "1", scaleX: "1", scaleY: "1", translateY: "0", translateX: '0'},
+        { duration: 80, delay: time });
+      time += 40;
+    });
+  }
+};
+var closeFABMenu = function (btn) {
+  let $this = btn;
+  // Get direction option
+  var horizontal = $this.hasClass('horizontal');
+  var offsetY, offsetX;
+
+  if (horizontal === true) {
+    offsetX = 40;
+  } else {
+    offsetY = 40;
+  }
+
+  $this.removeClass('active');
+  var time = 0;
+  $this.find('ul .btn-floating').velocity("stop", true);
+  $this.find('ul .btn-floating').velocity(
+    { opacity: "0", scaleX: ".4", scaleY: ".4", translateY: offsetY + 'px', translateX: offsetX + 'px'},
+    { duration: 80 }
+  );
 };
 
-function insertStartupData(callback) {
-  DiamondAPI.insert({
-    collection: 'postIt',
-    obj: {
-      title: '',
-      description: '',
-    },
-    callback,
-  });
-}
-
-function updatePostIt(e, key) {
-  DiamondAPI.update({
-    collection: 'postIt',
-    filter: {},
-    updateQuery: {
-      $set: {
-        [key]: e.value
-      }
-    },
-  });
-}
-
-function handleNewData(data) {
-  pushData('title', data.title);
-  pushData('description', data.description);
-
-  function pushData(e, value) {
-    let elem = document.getElementById(e);
-    elem.value = value;
+$(document).on('click.fixedActionBtn', '.fixed-action-btn.click-to-toggle > a', function(e) {
+  let $this = $(this);
+  let $menu = $this.parent();
+  
+  if($menu.hasClass('active')) {
+    closeFABMenu($menu);
+  } else {
+    openFABMenu($menu);
   }
-}
+});
