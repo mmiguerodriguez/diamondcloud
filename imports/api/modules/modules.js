0feb17bc9d1e97a77e21eaed72d5565e7475b386
Ryanitz <ryanitzcovitz@gmail.com>
2016-09-23 14:14:13 +0000

    Started file manager.

--- imports/api/modules/modules.js
+++ imports/api/modules/modules.js
@@ -9,5 +9,12 @@ export let createdModules = [
     img: "/modules/Jjwjg6gouWLXhMGKW/image.png",
     description: "I\"m a post it",
     validated: true,
+  },
+  {
+    _id: "p2dHVHDdfer9twe7r",
+    name: "File Manager",
+    img: "/modules/p2dHVHDdfer9twe7r/image.png",
+    description: "I\"m a file manager",
+    validated: true,
   }
 ];
