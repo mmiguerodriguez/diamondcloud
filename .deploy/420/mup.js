1de040f51baeb09026815729d71b318b8f4faeef
joelsobolmark <joelsobolmark@gmail.com>
2016-09-27 20:13:55 +0000

    corrected path to development project

---
+++ .deploy/420/mup.js
@@ -0,0 +1,41 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '104.131.158.182',
+      username: 'root',
+      // pem:
+      password: 'diamondcloud'
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'cloudDev',
+    path: '../../',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+    env: {
+      ROOT_URL: 'http://diamondcloud.tk:420',
+      PORT:'420',
+      // METEOR_DOWN_KEY: 'MY_SUPER_SECRET_KEY',
+      // ROOT_URL: 'http://159.203.182.182',
+      //MONGO_URL: 'http://diamondcloud.tk:420'
+    },
+
+    //dockerImage: 'kadirahq/meteord'
+    deployCheckWaitTime:60,
+    dockerImage: 'abernix/meteord:base',
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
