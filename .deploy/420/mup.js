534a2ec1d53fec14ea93989beabb3a58e6ebc91e
joelsobolmark <joelsobolmark@gmail.com>
2016-09-27 19:55:43 +0000

    added deploy folder

--- .deploy/420/mup.js
+++ .deploy/420/mup.js
@@ -10,8 +10,8 @@ module.exports = {
   },
 
   meteor: {
-    name: 'cloud',
-    path: './',
+    name: 'cloudDev',
+    path: '../../cloudDev',
     servers: {
       one: {}
     },
@@ -19,10 +19,11 @@ module.exports = {
       serverOnly: true,
     },
     env: {
-      ROOT_URL: 'http://diamondcloud.tk',
+      ROOT_URL: 'http://diamondcloud.tk:420',
+      PORT:'420',
       // METEOR_DOWN_KEY: 'MY_SUPER_SECRET_KEY',
       // ROOT_URL: 'http://159.203.182.182',
-      // MONGO_URL: '159.203.182.182'
+      //MONGO_URL: 'http://diamondcloud.tk:420'
     },
 
     //dockerImage: 'kadirahq/meteord'
