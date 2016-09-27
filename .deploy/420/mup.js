cf9dade8ec96dd194d32e2d48913d8d94617ae48
joelsobolmark <joelsobolmark@gmail.com>
2016-09-27 20:12:42 +0000

    corrected path to project

--- .deploy/420/mup.js
+++ .deploy/420/mup.js
@@ -11,7 +11,7 @@ module.exports = {
 
   meteor: {
     name: 'cloudDev',
-    path: '../../cloudDev',
+    path: '../../',
     servers: {
       one: {}
     },
--- .deploy/80/mup.js
+++ .deploy/80/mup.js
@@ -11,7 +11,7 @@ module.exports = {
 
   meteor: {
     name: 'cloud',
-    path: '../../cloud',
+    path: '../../',
     servers: {
       one: {}
     },
