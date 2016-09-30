fc8acba9565f2653723a2ad465d9626dd935a12d
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-09-30 23:08:45 +0000

    Continued with web-rtc module (a.k.a. Nothing works :)

--- public/modules/gkQwjmCTWbxZqRMbu/components/main.js
+++ public/modules/gkQwjmCTWbxZqRMbu/components/main.js
@@ -1,31 +1,45 @@
 const { DiamondAPI, SimplePeer, navigator } = window;
 var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
 
-if(getUserMedia) {
+if (getUserMedia) {
   getUserMedia = getUserMedia.bind(navigator);
 }
 
 var user = DiamondAPI.getCurrentUser();
 var users = DiamondAPI.getTeamData().users;
 
-// get video/voice stream
-getUserMedia({ video: true, audio: true }, gotMedia, function () {});
-
-function gotMedia (stream) {
-  var peer1 = new SimplePeer({ initiator: true, stream: stream });
-  var peer2 = new SimplePeer({ initiator: false });
-
-  peer1.on('signal', function (data) {
-    peer2.signal(data);
-  });
-
-  peer2.on('signal', function (data) {
-    peer1.signal(data);
-  });
-
-  peer2.on('stream', function (stream) {
-    // got remote video stream, now let's show it in a video tag
-    var video = document.querySelector('#their-video');
-    video.src = window.URL.createObjectURL(stream);
-  });
+function initiate(initiator) {
+  getUserMedia({ video: true, audio: true }, function (stream) {
+    var peer = new SimplePeer({
+      initiator,
+      trickle: false,
+      stream: stream
+    })
+  
+    peer.on('signal', function (data) {
+      document.getElementById('yourId').value = JSON.stringify(data)
+    })
+  
+    document.getElementById('connect').addEventListener('click', function () {
+      var otherId = JSON.parse(document.getElementById('otherId').value)
+      peer.signal(otherId)
+    })
+  
+    document.getElementById('send').addEventListener('click', function () {
+      var yourMessage = document.getElementById('yourMessage').value
+      peer.send(yourMessage)
+    })
+  
+    peer.on('data', function (data) {
+      document.getElementById('messages').textContent += data + '\n'
+    })
+  
+    peer.on('stream', function (stream) {
+      var video = document.createElement('video')
+      document.body.appendChild(video)
+  
+      video.src = window.URL.createObjectURL(stream)
+      video.play()
+    })
+  }, (error) => { console.log(error); })
 }
