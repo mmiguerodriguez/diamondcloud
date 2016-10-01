06e70a9fd48e9adcd5f4f90157b9f46ee04d559d
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-10-01 16:46:22 +0000

    Continued with p2p module, it works correctly :-)

--- public/modules/gkQwjmCTWbxZqRMbu/components/main.js
+++ public/modules/gkQwjmCTWbxZqRMbu/components/main.js
@@ -21,29 +21,107 @@ function initiate(initiator) {
       trickle: false,
     });
 
-    peer.on('signal', (data) => {
-      console.log('Received a signal', data);
-
-      document.getElementById('yourId').value = JSON.stringify(data);
-    });
-
     document.getElementById('connect').addEventListener('click', (event) => {
       let otherId = JSON.parse(document.getElementById('otherId').value);
       peer.signal(otherId);
 
       console.log('Connected to another peer', otherId);
     });
-
+    document.getElementById('disconnect').addEventListener('click', (event) => {
+      console.log('Disconnecting...');
+      peer.destroy();
+      
+      video.src = '';
+    });
+    
+    /* Messages (won't use for now)
     document.getElementById('send').addEventListener('click', (event) => {
       let message = document.getElementById('yourMessage').value;
       peer.send(message);
 
       console.log('Sent message', message);
     });
-
     peer.on('data', (data) => {
       console.log('Received data', data);
       document.getElementById('messages').textContent += data + '\n';
+    });
+    End messages */
+    
+    peer.on('signal', (data) => {
+      console.log('Received a signal', data);
+
+      DiamondAPI.get({
+        collection: 'users',
+        filter: {
+          '_id': user._id,
+        },
+        callback(error, result) {
+          if (error) {
+            console.log('Error getting users', error);
+          } else {
+            if(result.length > 0) {
+              DiamondAPI.update({
+                collection: 'users',
+                filter: {
+                  _id: user._id,
+                },
+                updateQuery: {
+                  $unset: {
+                    _id: "5e5pFqhaKa7gvj8Sz",
+                  },
+                },
+                callback(error, result) {
+                  if (error) {
+                    console.log('Error updating users', error);
+                  } else {
+                    console.log('Success removing user with ', user._id, ' id');
+                  }
+                }
+              });
+            }
+          }
+        }
+      });
+
+      DiamondAPI.insert({
+        collection: 'users',
+        obj: {
+          _id: user._id,
+          signal: data,
+        },
+        callback(error, success) {
+          if (error) {
+            console.log('Error inserting user signal', error);
+          } else {
+            console.log('Success inserting user signal', success);
+          }
+        }
+      });
+
+      document.getElementById('yourId').value = JSON.stringify(data);
+    });
+
+    peer.on('close', () => {
+      console.log('Closed connection');
+      
+      DiamondAPI.update({
+        collection: 'users',
+        filter: {
+          _id: user._id,
+        },
+        updateQuery: {
+          $unset: {
+            _id: user._id,
+          },
+        },
+        callback(error, result) {
+          if (error) {
+            console.log('Error updating', error);
+          } else {
+            console.log('Updated users collection correctly');
+          }
+        }
+      });
     });
 
     peer.on('stream', (stream) => {
@@ -56,3 +134,32 @@ function initiate(initiator) {
     console.log(error);
   });
 }
+
+DiamondAPI.subscribe({
+  request: {
+    collection: 'users',
+    condition: {},
+  },
+  callback(error, result) {
+    if (error) {
+      console.log('There was an error getting users data', error);
+    } else {
+      let users = result.users;
+      
+      console.log('Got user data correctly', users);
+      
+      let users_list = document.getElementById('users-list');
+      users_list.innerHTML = '';
+      
+      users.forEach((user) => {
+        users_list.innerHTML += `
+          <option 
+            value=${ JSON.stringify(user.signal) }>
+            ${ user._id }
+          </option>`;
+      });
+    }
+  }
+});
+
+
