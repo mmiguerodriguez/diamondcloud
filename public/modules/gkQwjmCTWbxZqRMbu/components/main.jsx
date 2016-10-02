554dfcc0c6e0ceb56b2c5729806f8fa31c6a2fe0
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-10-02 16:11:51 +0000

    Changed web-rtc library to SimpleWebRTC and made it work

--- public/modules/gkQwjmCTWbxZqRMbu/components/main.jsx
+++ public/modules/gkQwjmCTWbxZqRMbu/components/main.jsx
@@ -3,7 +3,7 @@ const {
   React,
   ReactDOM,
   ReactRouter,
-  SimplePeer,
+  SimpleWebRTC,
   classNames,
   navigator,
   URL,
@@ -29,363 +29,125 @@ browserHistory.push('/');
 class VideoChatPage extends React.Component {
   constructor() {
     super();
-
+    
     this.state = {
-      user: {}, // Actual user object
-      signal: {}, // Peer signal (another users' signals)
-      usersList: [], // Users list containing a dictionary of <_id, signal>
-
-      peer: null, // User peer
-
-      myVideo: null, // User video
-      videos: [], // Whole videos array
-
-      callStatus: 'Nada', // Call status (not working)
+      localVideoId: 'user-video',
+      videos: [],
+      webrtc: {},
     };
-
-    // Auto-bind functions that need binding
-    this.connect = this.connect.bind(this);
-    this.disconnect = this.disconnect.bind(this);
-    this.getPoopData = this.getPoopData.bind(this);
   }
   render() {
     return (
       <div>
-        <button onClick={ this.connect }>Connect</button>
-        <button onClick={ this.disconnect }>Disconnect</button>
-        <button onClick={ this.getPoopData }>Get signals</button>
-
-        <button onClick={ this.initiate.bind(this, true) }>Initiator</button>
-        <button onClick={ this.initiate.bind(this, false) }>Not initiator</button>
-
-        <select
-          id='users-list-select'
-          value={ JSON.stringify(this.state.signal) }
-          onChange={ this.handleChange.bind(this, 'signal') }>
-          { this.renderUsersOptions() }
-        </select>
-
-        <Video
-          id={ 'my-video' }
-          options={
-            {
-              autoplay: 'true',
-              muted: 'false',
-              width: '250px',
-              src: this.state.myVideo ? this.state.myVideo.src : '',
-            }
-          } />
-
-        { this.renderVideos() }
-
-        <p>{ this.state.callStatus }</p>
+        <UserVideo 
+          id={ this.state.localVideoId } 
+          width={ '250px' } />
+        <RemoteVideos 
+          videos={ this.state.videos } />
       </div>
     );
   }
+  componentWillMount() {
+    let self = this;
+    
+    let webrtc = new SimpleWebRTC({
+      localVideoEl: self.state.localVideoId,
+      remoteVideosEl: '',
+      autoRequestMedia: true,
+      nick: DiamondAPI.getCurrentUser().profile.name,
+    });
+    
+    self.setState({
+      webrtc,
+    });
+  }
   componentDidMount() {
     let self = this;
-
-    // Set team and user data
-    self.setState({
-      user: DiamondAPI.getCurrentUser(),
-      myVideo: {},
-    });
-
-    // Subscribe to users data and set signal to the first signal it finds
-    DiamondAPI.subscribe({
-      request: {
-        collection: 'users',
-        condition: {},
-      },
-      callback(error, result) {
-        if (error) {
-          console.log('There was an error getting users data', error);
-        } else {
-          let users = result.users;
-          console.log('Got user data correctly', users);
-
-          self.setState({
-            usersList: users,
-            signal: users[0].signal,
-          });
-        }
-      }
-    });
-  }
-
-  // Fake subscribe
-  getPoopData() {
-    let self = this;
-
-    DiamondAPI.get({
-      collection: 'users',
-      filter: {},
-      callback(error, users) {
-        if (error) {
-          console.log('Error getting user data', error);
-        } else {
-          console.log('Get poop data users', users);
-          self.setState({
-            signal: users[0].signal,
-            usersList: users,
-          });
-        }
-      }
-    });
-  }
-
-  // Connects to another peer
-  connect() {
-    console.log('Connecting to...', this.state.signal);
-    this.state.peer.signal(this.state.signal);
-  }
-  // Disconnects from peer
-  disconnect() {
-    this.state.peer.destroy(() => {
-      console.log('On disconnect..');
-
-      DiamondAPI.update({
-        collection: 'users',
-        filter: {
-          _id: self.state.user._id,
-        },
-        updateQuery: {
-          $unset: {
-            _id: self.state.user._id,
-          },
-        },
-        callback(error, result) {
-          if (error) {
-            console.log('Error closing cornnection (update)', error);
-          } else {
-            console.log('Updated users collection correctly');
-          }
-        }
-      });
-    });
-  }
-  // Adds peer to current connection
-  addPeer({ src }) {
-    let videos = this.state.videos;
-
-    videos.push({
-      id: 'random_id',
-      src,
-    });
-
-    this.setState({
-      videos,
-    });
-  }
-
-  // Initiates peer and sets its callbacks
-  // Ask for userMedia
-  initiate(isInitiator) {
-    let self = this;
-
-    getUserMedia({ video: true, audio: true }, (stream) => {
-      console.log('My stream (initialization)', stream);
-
-      this.state.myVideo.src = window.URL.createObjectURL(stream);
-
-      let peer = new SimplePeer({
-        initiator: isInitiator,
-        stream,
-        trickle: false,
-        config: {
-          iceServers: [
-            {
-              url: 'stun:23.21.150.121',
-            },
-            {
-              url: 'stun.l.google.com:19302',
-            },
-            {
-              url: 'stun1.l.google.com:19302',
-            },
-            {
-              url: 'stun2.l.google.com:19302',
-            },
-            {
-              url: 'stun3.l.google.com:19302',
-            },
-            {
-              url: 'stun4.l.google.com:19302',
-            },
-          ]
-        }
-      });
-
-      peer.on('signal', (data) => {
-        console.log('Received a signal', data);
-
-        if (data.type === 'offer') {
-          self.setState({
-            callStatus: 'Atendeeeeeee',
-          });
-        } else if (data.type === 'answer') {
-          self.setState({
-            callStatus: 'Llamandoooooo',
-          });
-        }
-
-        DiamondAPI.get({
-          collection: 'users',
-          filter: {
-            _id: self.state.user._id,
-          },
-          callback(error, result) {
-            if (error) {
-              console.log('Error getting users', error);
-            } else {
-              if(result.length > 0) {
-                console.log('User is inserted proceeding to update its collection');
-
-                // Set flags so update works correctly
-                data.$flags = {
-                  insertAsPlainObject: true,
-                };
-
-                DiamondAPI.update({
-                  collection: 'users',
-                  filter: {
-                    _id: self.state.user._id,
-                  },
-                  updateQuery: {
-                    $set: {
-                      signal: data,
-                    },
-                  },
-                  callback(error, result) {
-                    if (error) {
-                      console.log('Error updating user signal', error);
-                    } else {
-                      console.log('Success updating user signal for id', self.state.user._id);
-                    }
-                  }
-                });
-              } else {
-                console.log('User isn\'t inserted in the collection, inserting');
-
-                DiamondAPI.insert({
-                  collection: 'users',
-                  obj: {
-                    _id: self.state.user._id,
-                    signal: data,
-                  },
-                  callback(error, success) {
-                    if (error) {
-                      console.log('Error inserting user signal', error);
-                    } else {
-                      console.log('Success inserting user signal', success);
-                    }
-                  }
-                });
-              }
-            }
-          }
-        });
-      });
-      peer.on('data', (data) => {
-        console.log('Received a message with data', data);
-      });
-      peer.on('stream', (stream) => {
-        let src = URL.createObjectURL(stream);
-
-        self.addPeer({ src });
-      });
-
-      peer.on('close', () => {
-        console.log('Closed connection...'); // For some reason this never gets called
-      });
-
-      self.setState({
+    let webrtc = self.state.webrtc;
+    let currentBoard = DiamondAPI.getCurrentBoard();
+    
+    webrtc.on('videoAdded', (video, peer) => {
+      let videos = self.state.videos;
+      videos.push({
+        id: 'container_' + webrtc.getDomId(peer),
         peer,
       });
-    }, (error) => {
-      console.log('There was an error getting user media', error);
+      
+      self.setState({
+        videos,
+      });
     });
-  }
-
-  // Renders videos
-  renderVideos() {
-    if(this.state.videos) {
-      if(this.state.videos.length > 0) {
-        return this.state.videos.map((video) => {
-          return (
-            <Video
-              id={ video._id }
-              options={{
-                autoplay: true,
-                muted: false,
-                width: '',
-                src: video.src
-              }} />
-          );
-        });
-      } else {
-        return;
-      }
-    } else {
-      return;
-    }
-  }
-  // Renders <option>
-  renderUsersOptions() {
-    console.log('UsersList', this.state.usersList);
-    if (this.state.usersList) {
-      if (this.state.usersList.length > 0) {
-        let users = DiamondAPI.getTeamData().users;
-
-        return this.state.usersList.map((user) => {
-          if(this.state.user._id !== user._id) {
-            let user_name;
-            users.forEach((_user) => {
-              if(user._id === _user._id) {
-                user_name = _user.profile.name;
-              }
-            });
-
-            return (
-              <option
-                value={ JSON.stringify(user.signal) }>
-                { user_name }
-              </option>
-            );
-          } else {
-            return;
-          }
-        });
-      } else {
-        return;
-      }
-    } else {
-      return;
-    }
-  }
-  // Handles <select> change
-  handleChange(index, event) {
-    let value = event.target.value;
-    if (index === 'signal') {
-      value = JSON.parse(value);
-    }
-
-    this.setState({
-      [index]: value,
+    webrtc.on('videoRemoved', (video, peer) => {
+      let videos = self.state.videos;
+      let videoId = 'container_' + webrtc.getDomId(peer);
+      
+      videos.forEach((video, index) => {
+        if (video.id === videoId) {
+          videos.splice(index, 1);
+        }
+      });
+      
+      self.setState({
+        videos,
+      });
     });
+    
+    // Join to the currentBoard room
+    webrtc.joinRoom(currentBoard._id);
   }
 }
 
-// Component to render a single <video> element
+class UserVideo extends React.Component {
+  render() {
+    return (
+      <video
+        id={ this.props.id }
+        width={ this.props.width }>
+      </video>
+    );
+  }
+}
+
+class RemoteVideos extends React.Component {
+  render() {
+    return ( 
+      <div>
+        { this.renderVideos() }
+      </div>
+    );
+  } 
+  renderVideos() {
+    return this.props.videos.map((video) => {
+      return (
+        <Video { ...video } />
+      );
+    });
+  }
+}
 class Video extends React.Component {
   render() {
     return (
       <video
         id={ this.props.id }
-        { ...this.props.options }></video>
+        src={ URL.createObjectURL(this.props.peer.stream) }
+        onContextMenu={ () => { return false; } }
+        autoPlay>
+      </video>
     );
   }
 }
+
+UserVideo.propTypes = { 
+  id: React.PropTypes.string.isRequired,
+  width: React.PropTypes.string.isRequired,
+};
+RemoteVideos.propTypes = {
+  videos: React.PropTypes.array.isRequired
+};
+Video.propTypes = { 
+  id: React.PropTypes.string.isRequired,
+  peer: React.PropTypes.object.isRequired,
+};
 
 // Render classes with router
 ReactDOM.render(
