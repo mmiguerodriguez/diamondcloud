a1864d3fda11aea2502898b28fb07fdfc366ff85
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-10-14 22:04:31 +0000

    Continued with the server-side routes

--- imports/startup/server/accounts-config.js
+++ imports/startup/server/accounts-config.js
@@ -1,4 +1,18 @@
-Accounts.onCreateUser(function(options, user){
+import { shareTeam } from '../../api/teams/methods.js';
+import { Teams }     from '../../api/teams/teams.js';
+
+/**
+ * Callback when a user is created.
+ * @param {Function} func
+ *  @param {Object} options
+ *  @param {Object} user
+ *    The actual user object that had been
+ *    created, and we are modifying.
+ * @returns {Object} user
+ *  The user we will be inserting to the
+ *  database.
+ */
+Accounts.onCreateUser((options, user) => {
   const service = Object.keys(user.services);
   let name = user.services[service].name;
   let email = user.services[service].email;
@@ -11,4 +25,27 @@ Accounts.onCreateUser(function(options, user){
   };
 
   return user;
+});
+
+/**
+ * Callback when an user logs in.
+ * @param {Function} func
+ *
+ * 
+ * TODO: Get the team url depending from the actual route
+ */
+Accounts.onLogin(() => {
+  console.log(Teams.find().fetch());
+  let team = Teams.findOne({ url: 'carlosydario' });
+  
+  if (team.users.length === 0) {
+    Teams.update({ _id: team._id }, {
+      $push: {
+        users: {
+          email: Meteor.user().email(),
+          hierarchy: 'sistemas',
+        },
+      },
+    });
+  }
 });
