6b3ff5d046a6da0bbd85859e70bfe42ffc7c3f03
Ryanitz <ryanitzcovitz@gmail.com>
2016-10-14 15:14:38 +0000

    Added ErrorMessage component but doesnt work

--- imports/ui/app/AppLayout.jsx
+++ imports/ui/app/AppLayout.jsx
@@ -1,8 +1,60 @@
 import React        from 'react';
 
 import NavbarLayout from '../navbar/NavbarLayout.jsx';
+import ErrorMessage from '../error-message/ErrorMessage.jsx';
+
+const ERROR_DELAY = 5000;
 
 export default class AppLayout extends React.Component {
+  /**
+   * Sets the error state so we can show an error
+   * correctly.
+   * @param {Object} object
+   *  @param {String} body
+   *   Error message.
+   *  @param {Number} delay
+   *   The delay until the message is closed
+   *  @param {Boolean} showing.
+   *   State to check if the message is being
+   *   shown or not.
+   */
+  showError({ body, delay }) {
+    this.setState({
+      error: {
+        body,
+        delay: delay ? delay : ERROR_DELAY,
+        showing: true,
+      },
+    });
+  }
+  /**
+   * Resets the error state to the default.
+   */
+  hideError() {
+    this.setState({
+      error: {
+        body: '',
+        delay: ERROR_DELAY,
+        showing: false,
+      },
+    });
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: {
+        body: '',
+        delay: ERROR_DELAY,
+        showing: false,
+      }
+    };
+    
+    this.showError = this.showError.bind(this);
+    this.hideError = this.hideError.bind(this);
+  }
+
   render() {
     return (
       <div>
@@ -12,8 +64,18 @@ export default class AppLayout extends React.Component {
         />
         { 
           React.cloneElement(this.props.children, { 
-            ...this.props 
+            ...this.props,
+            showError: this.showError,
+            hideError: this.hideError,
           })
+        }
+        {
+          this.state.error.showing ? (
+            <ErrorMessage
+              hideError={this.hideError}
+              {...this.state.error}
+            />
+          ) : (null)
         }
       </div>
     );
