127a12d93f44af49d4be08ebacaff3a206016958
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-10-16 11:02:51 -0300

    Made instant lock-unlock for boards

--- imports/ui/team/board/Board.jsx
+++ imports/ui/team/board/Board.jsx
@@ -1,73 +1,21 @@
 import React          from 'react';
 import classNames     from 'classnames';
 
-import ModuleInstance from '../../module-instance/ModuleInstance.jsx';
+import ModuleInstance from '../../module-instance/ModuleInstance';
 
 export default class Board extends React.Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      visibleForDirectors: this.props.board.visibleForDirectors,
+    };
+
     this.toggleBoardToDirectors = this.toggleBoardToDirectors.bind(this);
   }
 
-  render() {
-    let classes = classNames('board-container', {
-      'permission-asker-opened': this.props.permissionAsker
-    });
-    return (
-      <div className={ classes }>
-        <div className='sub-header'>
-          <div className='sub-header-data col-xs-6'>
-            <ol className="breadcrumb truncate">
-              <li><a href="#">{ this.props.team.name }</a></li>
-              <li className="active">{ this.props.board.name }</li>
-            </ol>
-            { /* <h4 className='title truncate'>{ this.props.board.name }</h4> */ }
-          </div>
-          <div className='col-xs-6 right-data'>
-            <h4 className='members truncate'>
-              {this.renderUsers()}
-            </h4>
-            <div className="visibility">
-              {
-                this.props.board.visibleForDirectors ? (
-                  <img
-                    role="button"
-                    onClick={() => this.toggleBoardToDirectors('lockBoard')}
-                    src="/img/visibility-off.svg"
-                    className="visibility-img"
-                    title="Hacer no visible para directores"
-                  />
-                ) : (
-                  <img
-                    role="button"
-                    onClick={() => this.toggleBoardToDirectors('unlockBoard')}
-                    src="/img/visibility-on.svg"
-                    className="visibility-img"
-                    title="Hacer visible para directores"
-                  />
-                )
-              }
-            </div>
-            <span className='message-icon-span' onClick={ this.props.addChat.bind(null, { boardId: this.props.board._id }) }>
-              { /* <h4 className='message-text'>Chat del board</h4> */ }
-              <img
-                src='/img/sidebar/messages.svg'
-                title='Abrir chat del board'
-                className='message-icon'
-                width='28px'/>
-            </span>
-          </div>
-        </div>
-        <div className='board'>
-          { this.renderModules() }
-        </div>
-      </div>
-    );
-  }
-
   componentDidMount() {
-    let self = this;
+    const self = this;
 
     $('.board').droppable({
       accept(e) {
@@ -78,15 +26,15 @@ export default class Board extends React.Component {
         return valid;
       },
       drop(event, ui) {
-        let item = ui.draggable.hasClass('module-item');
-        let container = ui.draggable.hasClass('module-container');
+        const item = ui.draggable.hasClass('module-item');
+        const container = ui.draggable.hasClass('module-container');
 
         if (item) {
-          let boardId = self.props.board._id;
-          let moduleId = ui.draggable.data('module-id');
+          const boardId = self.props.board._id;
+          const moduleId = ui.draggable.data('module-id');
 
-          let x = ui.position.top - 40;
-          let y = ui.position.left;
+          const x = ui.position.top - 40;
+          const y = ui.position.left;
 
           if (x >= 0 && y >= 0) {
             Meteor.call('ModuleInstances.methods.create', {
@@ -107,10 +55,10 @@ export default class Board extends React.Component {
             console.error('Can\'t create module on those coordinates.');
           }
         } else if (container) {
-          let moduleInstanceId = ui.draggable.data('moduleinstance-id');
+          const moduleInstanceId = ui.draggable.data('moduleinstance-id');
 
-          let x = ui.position.top;
-          let y = ui.position.left;
+          const x = ui.position.top;
+          const y = ui.position.left;
 
           if (x >= 0 && y >= 0) {
             Meteor.call('ModuleInstances.methods.edit', {
@@ -128,15 +76,65 @@ export default class Board extends React.Component {
             console.error('Can\'t create module on those coordinates.');
           }
         }
+      },
+    });
+  }
+
+  toggleBoardToDirectors(methodName) {
+    const self = this;
+
+    this.setState({
+      visibleForDirectors: methodName === 'unlockBoard',
+    });
+
+    Meteor.call(`Boards.methods.${methodName}`, {
+      _id: this.props.board._id,
+    }, (error, result) => {
+      if (error) {
+        console.error(error);
+        // If there was an error, reset state to the correct one.
+        self.setState({
+          visibleForDirectors: methodName === 'unlockBoard',
+        });
       }
     });
   }
 
-  renderModules() {
-    let arr = [];
+  renderUsers() {
+    if (this.props.board.isPrivate) {
+      return this.props.board.users.map((_user) => {
+        const user = Meteor.users.findByEmail(_user.email, {}) || _user;
 
+        return (
+          <img
+            key={user._id || _user.email}
+            className="img-circle shared-people"
+            src={user.profile ? user.profile.picture : '/img/user-shape.svg'}
+            title={user.profile ? user.profile.name : _user.email}
+            width="32px"
+          />
+        );
+      });
+    } else {
+      return this.props.team.users.map((_user) => {
+        const user = Meteor.users.findByEmail(_user.email, {}) || _user;
+
+        return (
+          <img
+            key={user._id || _user.email}
+            className="img-circle shared-people"
+            src={user.profile ? user.profile.picture : '/img/user-shape.svg'}
+            title={user.profile ? user.profile.name : user.email}
+            width="32px"
+          />
+        );
+      });
+    }
+  }
+
+  renderModules() {
     if (this.props.moduleInstances) {
-      this.props.moduleInstances.map((moduleInstance) => {
+      return this.props.moduleInstances.map((moduleInstance) => {
 
         let module;
         this.props.modules.forEach((_module) => {
@@ -145,64 +143,86 @@ export default class Board extends React.Component {
           }
         });
 
-        arr.push(
+        return (
           <ModuleInstance
-            key={ moduleInstance._id }
-            moduleInstance={ moduleInstance }
-            moduleInstancesFrames={ this.props.moduleInstancesFrames }
-            module={ module }
-            boards={ this.props.boards }
-            users={ this.props.users }
-            openModuleInstanceContextMenu={ this.props.openModuleInstanceContextMenu }
-            />
+            key={moduleInstance._id}
+            moduleInstance={moduleInstance}
+            moduleInstancesFrames={this.props.moduleInstancesFrames}
+            module={module}
+            boards={this.props.boards}
+            users={this.props.users}
+            openModuleInstanceContextMenu={this.props.openModuleInstanceContextMenu}
+          />
         );
       });
     }
 
-    return arr;
+    return (null);
   }
 
-  renderUsers() {
-    if (this.props.board.isPrivate) {
-      return this.props.board.users.map((_user) => {
-        let user = Meteor.users.findByEmail(_user.email, {}) || _user;
+  render() {
+    const classes = classNames('board-container', {
+      'permission-asker-opened': this.props.permissionAsker,
+    });
 
-        return (
-          <img
-            key={ user._id || _user.email }
-            className='img-circle shared-people'
-            src={ user.profile ? user.profile.picture : '/img/user-shape.svg' }
-            title={ user.profile ? user.profile.name : _user.email }
-            width='32px' />
-        );
-      });
-    } else {
-      return this.props.team.users.map((_user) => {
-        let user = Meteor.users.findByEmail(_user.email, {}) || _user;
-
-        return (
-          <img
-            key={ user._id || _user.email }
-            className='img-circle shared-people'
-            src={ user.profile ? user.profile.picture : '/img/user-shape.svg'  }
-            title={ user.profile ? user.profile.name : user.email }
-            width='32px' />
-        );
-      });
-    }
-  }
-
-  toggleBoardToDirectors(methodName) {
-    Meteor.call(
-      `Boards.methods.${methodName}`,
-      {
-        _id: this.props.board._id
-      },
-      (err, res) => {
-        if (err) {
-          throw new console.error(err);
-        }
-      }
+    return (
+      <div className={classes}>
+        <div className="sub-header">
+          <div className="sub-header-data col-xs-6">
+            <ol className="breadcrumb truncate">
+              <li>
+                <a href="">{this.props.team.name}</a>
+              </li>
+              <li className="active">{this.props.board.name}</li>
+            </ol>
+            { /* <h4 className='title truncate'>{ this.props.board.name }</h4> */ }
+          </div>
+          <div className="col-xs-6 right-data">
+            <h4 className="members truncate">
+              {this.renderUsers()}
+            </h4>
+            <div className="visibility">
+              {
+                this.state.visibleForDirectors ? (
+                  <img
+                    role="button"
+                    onClick={() => this.toggleBoardToDirectors('lockBoard')}
+                    src="/img/visibility-off.svg"
+                    className="visibility-img"
+                    title="Hacer no visible para directores"
+                    alt="Hacer no visible para directores"
+                  />
+                ) : (
+                  <img
+                    role="button"
+                    onClick={() => this.toggleBoardToDirectors('unlockBoard')}
+                    src="/img/visibility-on.svg"
+                    className="visibility-img"
+                    title="Hacer visible para directores"
+                    alt="Hacer visible para directores"
+                  />
+                )
+              }
+            </div>
+            <span
+              className="message-icon-span"
+              onClick={this.props.addChat.bind(null, {
+                boardId: this.props.board._id
+              })}>
+              { /* <h4 className='message-text'>Chat del board</h4> */ }
+              <img
+                src="/img/sidebar/messages.svg"
+                title="Abrir chat del board"
+                className="message-icon"
+                width="28px"
+              />
+            </span>
+          </div>
+        </div>
+        <div className="board">
+          { this.renderModules() }
+        </div>
+      </div>
     );
   }
 }
