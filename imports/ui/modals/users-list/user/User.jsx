9c044b16ce1d99672dedfca9f3dc7a2cff48093e
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-10-16 23:35:40 -0300

    Fixed issues with UserList images and fixed code to prevent lint errors

--- imports/ui/modals/users-list/user/User.jsx
+++ imports/ui/modals/users-list/user/User.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export default class UsersList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      hierarchy: this.props.user.hierarchy || 'sistemas',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
   handleChange(index, event) {
     const self = this;
     const value = event.target.value;
@@ -12,7 +22,7 @@ export default class UsersList extends React.Component {
         hierarchy: value,
       }, (error, result) => {
         if (error) {
-          console.error(error);
+          // console.error(error);
         }
       });
     }
@@ -22,16 +32,6 @@ export default class UsersList extends React.Component {
     });
   }
 
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      hierarchy: this.props.user.hierarchy || 'sistemas',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
   render() {
     return (
       <div className="row">
@@ -39,7 +39,7 @@ export default class UsersList extends React.Component {
           <img
             className="contact-list-photo"
             alt="User"
-            src={`${this.props.user.profile.picture}?sz=60` || '/img/user-shape.svg'}
+            src={this.props.user.profile.picture ? `${this.props.user.profile.picture}?sz=60` : '/img/user-shape.svg'}
           />
         </div>
         <div className="col-xs-10">
@@ -50,14 +50,15 @@ export default class UsersList extends React.Component {
             id="user-type-edit"
             className="form-control user-type edit"
             value={this.state.hierarchy}
-            onChange={(e) => this.handleChange('hierarchy', e)}>
-            <option value='sistemas'>Sistemas</option>
-            <option value='creativo'>Creativo</option>
-            <option value='director creativo'>Director creativo</option>
-            <option value='director de cuentas'>Director de cuentas</option>
-            <option value='coordinador'>Coordinador</option>
-            <option value='administrador'>Administrador</option>
-            <option value='medios'>Medios</option>
+            onChange={e => this.handleChange('hierarchy', e)}
+          >
+            <option value="sistemas">Sistemas</option>
+            <option value="creativo">Creativo</option>
+            <option value="director creativo">Director creativo</option>
+            <option value="director de cuentas">Director de cuentas</option>
+            <option value="coordinador">Coordinador</option>
+            <option value="administrador">Administrador</option>
+            <option value="medios">Medios</option>
           </select>
         </div>
         {
@@ -65,7 +66,8 @@ export default class UsersList extends React.Component {
             <div className="col-xs-1">
               <div
                 className="close"
-                onClick={this.props.removeUser.bind(null, this.props.user.emails[0].address)}>
+                onClick={this.props.removeUser.bind(null, this.props.user.emails[0].address)}
+              >
                 <img src="/img/close-icon.svg" width="16px" />
               </div>
             </div>
