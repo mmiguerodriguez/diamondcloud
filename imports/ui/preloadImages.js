b4c4c5f8890e2d7a5b87c4412f1e202de1990b98
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-10-20 18:26:40 -0300

    Added all images to preload

--- imports/ui/preloadImages.js
+++ imports/ui/preloadImages.js
@@ -10,7 +10,7 @@ const preloadImages = () => {
     '/img/check-symbol.svg',
     '/img/close-icon.svg',
     '/img/cyd-landing-background.jpg',
-    '/img/dashboard.png',
+    // '/img/dashboard.png',
     '/img/google-icon.png',
     '/img/logo-cyd.jpg',
     '/img/logo.ico',
@@ -25,8 +25,10 @@ const preloadImages = () => {
     '/img/user-shape.jpg',
     '/img/visibility-off.svg',
     '/img/visibility-on.svg',
+    // chat icons
     '/img/chat/back-arrow.svg',
     '/img/chat/maximize.svg',
+    // sidebar icons
     '/img/sidebar/boards.svg',
     '/img/sidebar/config.svg',
     '/img/sidebar/messages.svg',
@@ -34,6 +36,15 @@ const preloadImages = () => {
     '/img/sidebar/notification-badge.svg',
     '/img/sidebar/shop-cart.svg',
     '/img/sidebar/vertical-ellipsis.svg',
+    // modules previews & icons
+    '/modules/drive/image.png',
+    '/modules/post-it/image.png',
+    '/modules/task-manager/image.png',
+    '/modules/videocall/image.png',
+    '/modules/drive/preview.png',
+    '/modules/post-it/preview.png',
+    '/modules/task-manager/preview.png',
+    '/modules/videocall/preview.png',
   ];
 
   IMAGES.forEach((src) => {
