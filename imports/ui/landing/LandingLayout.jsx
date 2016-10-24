bbe74b8d0c11079d48f2155d268e48354a5b497e
miguel.rodriguez <mmiguerodriguez@gmail.com>
2016-10-24 17:10:41 -0300

    Added custom landing for /diamond

--- imports/ui/landing/LandingLayout.jsx
+++ imports/ui/landing/LandingLayout.jsx
@@ -1,17 +1,32 @@
 import React             from 'react';
+import classNames        from 'classnames';
 
 import AccountsUIWrapper from '../accounts/AccountsUIWrapper';
 
 export default class LandingLayout extends React.Component {
   render() {
+    const isCyD = this.props.location.pathname === '/carlosydario';
+    const backgroundClass = classNames({
+      diamond: !isCyD,
+      cyd: isCyD,
+    }, 'background');
+    const logoSrc = classNames({
+      '/img/logo.png': !isCyD,
+      '/img/logo-cyd.jpg': isCyD,
+    });
+
     return (
       <div className="landing">
-        <div className="background" />
+        <div className={backgroundClass} />
         <div className="items-container">
-          <img className="title" src="/img/logo-cyd.jpg" />
+          <img className="title" src={logoSrc} />
           <AccountsUIWrapper />
         </div>
       </div>
     );
   }
 }
+
+LandingLayout.propTypes = {
+  location: React.PropTypes.object.isRequired,
+};
