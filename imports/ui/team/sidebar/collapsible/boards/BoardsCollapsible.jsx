a077519911ee0f151a4c35223e677c4d4e62a79f
Ryanitz <ryanitzcovitz@gmail.com>
2016-11-02 21:29:05 +0000

    Fixed permissions with board type = medios

--- imports/ui/team/sidebar/collapsible/boards/BoardsCollapsible.jsx
+++ imports/ui/team/sidebar/collapsible/boards/BoardsCollapsible.jsx
@@ -57,7 +57,7 @@ export default class BoardsCollapsible extends React.Component {
       );
 
       if (isDirector) {
-        if ((board.type === 'creativos' && board.visibleForDirectors) || (board.type !== 'creativos')) {
+        if ((board.type === 'creativos' && board.visibleForDirectors) || (board.type !== 'creativos' && board.type !== 'medios')) {
           boards.push(_board);
         }
       } else {
