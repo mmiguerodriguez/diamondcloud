c875d4a5793c0fbccfee9579d45ac1e28b5806c1
Joel Sobol Mark <joelsobolmark@gmail.com>
2016-11-06 23:51:34 +0000

    Continued working on Event creation

--- public/modules/calendar/main.jsx
+++ public/modules/calendar/main.jsx
@@ -13,6 +13,27 @@ const {
   browserHistory,
 } = ReactRouter;
 
+/*
+
+Events
+  - _id
+  - title
+  - description
+  - startDate
+  - endDate
+  - isPrivate
+  - boards (<=> isPrivate)
+  - color
+
+*/
+
+// Do NOT change function to ES6
+Date.prototype.addDays = function(days) {
+  let date = new Date(this.valueOf());
+  date.setDate(date.getDate() + days);
+  return date;
+}
+
 const isCoordination = (board) => {
   return (
     board.type === 'coordinadores' ||
@@ -29,7 +50,7 @@ class EventsList extends React.Component {
   getEvents() {
     return this.props.events.map((event) => {
       return (
-        <li>{event.name}</li>
+        <li>{event.title}: {event.description}</li>
       );
     });
   }
@@ -51,20 +72,45 @@ class CreateEventTab extends React.Component {
       [index]: value,
     });
   }
+  
+  handleRadio(index, event) {
+    let value = event.target.value;
+
+    this.setState({
+      isPrivate: index === 'private',
+    });
+  }
 
   createEvent() {
-    alert(this.state.title, this.state.description);
+    console.log(this.state.isPrivate);
+
+    DiamondAPI.insert({
+      collection: 'events',
+      object: {
+        title: this.state.title,
+        description: this.state.description,
+        isPrivate: this.state.isPrivate,
+        startDate: (new Date()).addDays(3),
+        endDate: (new Date()).addDays(4),
+      },
+      isGlobal: true,
+      callback: (err, res) => {
+        this.props.backFunction();
+      }
+    });
   }
 
   constructor(props) {
     super(props);
-    
+
     this.state = {
       title: '',
       description: '',
+      isPrivate: false,
     };
-    
+
     this.createEvent = this.createEvent.bind(this);
+    this.handleRadio = this.handleRadio.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
@@ -75,14 +121,31 @@ class CreateEventTab extends React.Component {
           id="create-event-title"
           className="form-control"
           value={this.state.title}
+          placeholder="Título"
           onChange={this.handleChange.bind(null, 'title')}
         />
         <input
           id="create-event-description"
           className="form-control"
           value={this.state.description}
+          placeholder="Descripción"
           onChange={this.handleChange.bind(null, 'description')}
         />
+        <input
+          defaultChecked={true}
+          type="radio"
+          name="isPrivate"
+          value="public"
+          onChange={e => this.handleRadio('public', e)}
+        />
+        Público
+        <input
+          type="radio"
+          name="isPrivate"
+          value="private"
+          onChange={e => this.handleRadio('private', e)}
+        />
+        Privado
         <button onClick={this.createEvent}>Create event</button>
       </div>
     );
@@ -100,6 +163,7 @@ class Calendar extends React.Component {
     };
     
     this.openCreateEvent = this.openCreateEvent.bind(this);
+    this.closeCreateEvent = this.closeCreateEvent.bind(this);
   }
 
   componentDidMount() {
@@ -125,6 +189,12 @@ class Calendar extends React.Component {
     });
   }
   
+  closeCreateEvent() {
+    this.setState({
+      currentTab: 'events',
+    });
+  }
+  
   render() {
     if (this.state.loading) {
       return (
@@ -138,14 +208,21 @@ class Calendar extends React.Component {
       <div>
         {
           isCoordination(DiamondAPI.getCurrentBoard()) ?
-          <button onClick={this.openCreateEvent}>Create event</button> : null
+          (
+            this.state.currentTab === 'events' ?
+            <button onClick={this.openCreateEvent}>Create event</button> :
+            <button onClick={this.closeCreateEvent}>Back to events</button>
+          ) :
+          null
         }
         {
           this.state.currentTab === 'events' ?
           <EventsList
             events={this.state.events}
           /> :
-          <CreateEventTab/>
+          <CreateEventTab
+            backFunction={this.closeCreateEvent}
+          />
         }
       </div>
     );
