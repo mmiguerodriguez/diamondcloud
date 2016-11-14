392acf83a30586695b3318fe82a8812acc7f11cf
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-11-14 19:26:24 +0000

    Made post it with webpack and fixed issue with archived tasks on task-manager module

---
+++ public/modules/post-it/webpack.development.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const loaders = require('./webpack.loaders');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WebpackCleanupPlugin = require('webpack-cleanup-plugin');
+
+module.exports = {
+	entry: [
+	  './src/main.css',
+		'./src/main.js',
+	],
+	output: {
+		path: path.join(__dirname, 'dev'),
+		filename: '[chunkhash].js',
+	},
+	resolve: {
+		extensions: ['', '.js', '.jsx'],
+	},
+	module: {
+		loaders,
+	},
+	plugins: [
+		new WebpackCleanupPlugin(),
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: '"development"',
+			},
+		}),
+		new ExtractTextPlugin('[contenthash].css'),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			title: 'Webpack example',
+		}),
+	],
+  externals: {
+    react: 'React',
+    'react-dom': 'ReactDOM',
+    'react-router': 'ReactRouter',
+    classNames: 'classNames',
+  },
+};
