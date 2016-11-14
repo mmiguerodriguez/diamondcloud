8f2bfae75d8a923200655437d9ade358a5d26983
miguelrodriguez <mmiguerodriguez@gmail.com>
2016-11-14 19:48:53 +0000

    Added check when building a module

--- build-modules.sh
+++ build-modules.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+if [[ $1 != "development" && $1 != "production" ]]; then
+  echo 'The environment you are trying to build doesnt exists'
+  exit 1
+fi
+
 # Function build
 # params {String} module
 # params {String} build
+# Calls the webpack function to build a module
 function build {
   # Open the folder
   cd $1
@@ -10,7 +16,7 @@ function build {
   if [ ! -f webpack.$2.config.js ]; then
     # If the build doesn't exists then show an error message
     # and return to the current folder
-    echo 'File webpack.'$2'.config.js in '$1' doesnt exists, watch out!'
+    echo 'File webpack.'$2'.config.js in '$1' doesnt exists'
     cd ..
     return
   fi
@@ -23,6 +29,7 @@ function build {
 # Open the modules folders
 cd public/modules
 # Iterate for each module folder we find
+
 for module in */ ; do
   # Call the build function with the module folder name
   # and the parameter we passed to the script
