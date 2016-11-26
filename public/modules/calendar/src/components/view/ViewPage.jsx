993b0db4edee01a8bb6747c1ccf3ffa5eabb915f
Ryanitz <ryanitzcovitz@gmail.com>
2016-11-26 18:02:34 +0000

    Made commit to continue local and not in c9

--- public/modules/calendar/src/components/view/ViewPage.jsx
+++ public/modules/calendar/src/components/view/ViewPage.jsx
@@ -4,8 +4,12 @@ class ViewPage extends React.Component {
   render() {
     return (
       <div>
-        <div className='calendar-navbar' />
-        <div id='calendar'></div>
+        <div className='calendar-navbar'>
+          <h3 className='fixed'>Calendario</h3>
+          <div className='navbar-btn config' />
+          <div className='navbar-btn create' />
+        </div>
+        <div id='calendar' />
       </div>
     );
   }
@@ -18,6 +22,26 @@ class ViewPage extends React.Component {
           center: 'title',
           right:  'month,list'
         },
+        eventBackgroundColor: '#3498db',
+        eventTextColor: '#eee',
+        eventSources: [{
+          events: [
+            {
+              title  : 'event1',
+              start  : '2016-11-11'
+            },
+            {
+              title  : 'event2',
+              start  : '2016-11-13',
+              end    : '2016-11-18',
+            },
+            {
+              title  : 'event3',
+              start  : '2016-11-10T12:30:00',
+              allDay : false // will make the time show,
+            }
+          ],
+        }],
     });
   }
 }
