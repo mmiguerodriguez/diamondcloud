ba1718d4ccb3700122fdb245173da14040b894b3
migu3x <mmiguerodriguez@gmail.com>
2018-09-08 21:40:17 -0300

    Fixed all issues

--- server/kadira.js
+++
@@ -1,7 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Kadira } from 'meteor/meteorhacks:kadira';
-
-Kadira.connect(
-  Meteor.settings.private.kadira.appId,
-  Meteor.settings.private.kadira.appSecret
-);
